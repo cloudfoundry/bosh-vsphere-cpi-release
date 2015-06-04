d8650b85bdfd4880451183849e999aaecc953f31
Kam Leung <kleung@pivotal.io>
2015-06-04 13:46:20 -0700

    Sets up initial concourse pipeline
    Adds only a triggering build-candidate job
    
    [#73636468](https://www.pivotaltracker.com/story/show/73636468)
    Signed-off-by: Jonathan Barnes <jbarnes@pivotal.io>

---
+++ ci/tasks/build-candidate.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+set -e
+
+source /etc/profile.d/chruby-with-ruby-2.1.2.sh
+
+semver=`cat version-semver/number`
+
+mkdir out
+
+cd bosh-cpi-release
+
+echo "installing the latest bosh_cli"
+gem install bosh_cli --no-ri --no-rdoc
+
+echo "using bosh CLI version..."
+bosh version
+
+cpi_release_name="bosh-vsphere-cpi"
+
+echo "building CPI release..."
+bosh create release --name $cpi_release_name --version $semver --with-tarball
+
+mv dev_releases/$cpi_release_name/$cpi_release_name-$semver.tgz ../out/
