2f0117835d849f89ea9d3c19c45bfad347333b19
Kam Leung <kleung@pivotal.io>
2015-06-04 15:36:43 -0700

    Adds deploy and bats jobs for ubuntu/manual
    Note: This config has not be verified as the story was put on hold
    
    [#73636468](https://www.pivotaltracker.com/story/show/73636468)
    Signed-off-by: Jonathan Barnes <jbarnes@pivotal.io>

---
+++ ci/tasks/deploy.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env bash
+
+set -e -x
+
+ensure_not_replace_value() {
+  local name=$1
+  local value=$(eval echo '$'$name)
+  if [ "$value" == 'replace-me' ]; then
+    echo "environment variable $name must be set"
+    exit 1
+  fi
+}
+
+ensure_not_replace_value base_os
+ensure_not_replace_value network_type_to_test
+
+cpi_release_name=bosh-vsphere-cpi
+
+source /etc/profile.d/chruby-with-ruby-2.1.2.sh
+
+semver=`cat version-semver/number`
+manifest_dir=bosh-concourse-ci/pipelines/$cpi_release_name
+manifest_filename=$manifest_dir/$base_os-$network_type_to_test-director-manifest.yml
+
+echo "normalizing paths to match values referenced in $manifest_filename"
+mkdir ./tmp
+mv ./bosh-cpi-dev-artifacts/$cpi_release_name-$semver.tgz ./tmp/$cpi_release_name.tgz
+mv ./bosh-release/release.tgz ./tmp/bosh-release.tgz
+mv ./stemcell/stemcell.tgz ./tmp/stemcell.tgz
+
+initver=$(cat bosh-init/version)
+initexe="$PWD/bosh-init/bosh-init-${initver}-linux-amd64"
+chmod +x $initexe
+
+echo "deleting existing BOSH Director VM..."
+$initexe delete $manifest_filename
+
+echo "deploying BOSH..."
+$initexe deploy $manifest_filename
