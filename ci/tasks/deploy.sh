0174c9d9498dc66899b86c03a44f891b914012bc
Corey Innis <cinnis@pivotal.io>
2015-06-17 13:31:19 -0700

    Update task script to match changes in bosh-init
    ...specifically, the handling of relative paths to artifacts in
    deployment manifests.
    
    [#73636468](https://www.pivotaltracker.com/story/show/73636468)
    
    Signed-off-by: Tim Hausler <thausler@pivotal.io>

--- ci/tasks/deploy.sh
+++ ci/tasks/deploy.sh
@@ -21,12 +21,13 @@ source /etc/profile.d/chruby-with-ruby-2.1.2.sh
 semver=`cat version-semver/number`
 manifest_dir=bosh-concourse-ci/pipelines/$cpi_release_name
 manifest_filename=$manifest_dir/$base_os-$network_type_to_test-director-manifest.yml
+manifest_artifacts=$manifest_dir/tmp
 
 echo "normalizing paths to match values referenced in $manifest_filename"
-mkdir ./tmp
-mv ./bosh-cpi-dev-artifacts/$cpi_release_name-$semver.tgz ./tmp/$cpi_release_name.tgz
-mv ./bosh-release/release.tgz ./tmp/bosh-release.tgz
-mv ./stemcell/stemcell.tgz ./tmp/stemcell.tgz
+mkdir $manifest_artifacts
+mv ./bosh-cpi-dev-artifacts/$cpi_release_name-$semver.tgz $manifest_artifacts/$cpi_release_name.tgz
+mv ./bosh-release/release.tgz $manifest_artifacts/bosh-release.tgz
+mv ./stemcell/stemcell.tgz $manifest_artifacts/stemcell.tgz
 
 initver=$(cat bosh-init/version)
 initexe="$PWD/bosh-init/bosh-init-${initver}-linux-amd64"
