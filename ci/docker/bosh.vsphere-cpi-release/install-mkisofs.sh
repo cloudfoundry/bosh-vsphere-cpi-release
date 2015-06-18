946756efea495b937df7e7f9eb20028e0a452a87
Tim Hausler <thausler@pivotal.io>
2015-06-18 12:27:19 -0700

    Add mkisofs to the Dockerfile for vsphere-cpi-release lifecycle tests.
    [#93835728](https://www.pivotaltracker.com/story/show/93835728)
    
    Signed-off-by: Corey Innis <cinnis@pivotal.io>

---
+++ ci/docker/bosh.vsphere-cpi-release/install-mkisofs.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+source /etc/profile.d/chruby.sh
+chruby 2.1.2
+
+export BOSH_INSTALL_TARGET=/usr/local
+
+cd /tmp/cpi-release
+
+# Create a CPI dev release, which we use to install mkisofs packages.
+bosh create release --with-tarball
+
+cd packages/vsphere_cpi_mkisofs
+tar zxf ../../dev_releases/bosh-vsphere-cpi/*.tgz
+tar zxf packages/vsphere_cpi_mkisofs.tgz
+
+# Share the packaging script used by the CPI release.
+chmod +x ./packaging
+./packaging
+
+rm -rf /tmp/cpi-release
