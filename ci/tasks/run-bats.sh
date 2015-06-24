6a0b2bd7cd53297f00580407217c2fed299ffcf5
Megan Murawski <megan.hyland@emc.com>
2015-06-24 15:39:40 -0700

    Fixed the key location to use absolute path.
    Also, added check_param coverage for all params.
    
    [#73636458](https://www.pivotaltracker.com/story/show/73636458)
    
    Signed-off-by: Aaron Hurley <ahurley@pivotal.io>

--- ci/tasks/run-bats.sh
+++ ci/tasks/run-bats.sh
@@ -6,20 +6,43 @@ source bosh-cpi-release/ci/tasks/utils.sh
 
 check_param base_os
 check_param network_type_to_test
+check_param BAT_INFRASTRUCTURE
+check_param BAT_VCAP_PASSWORD
+check_param BAT_STEMCELL
+check_param BOSH_SSH_PRIVATE_KEY
+check_param BOSH_VSPHERE_NETMASK
+check_param BOSH_VSPHERE_GATEWAY
+check_param BOSH_VSPHERE_DNS
+check_param BOSH_VSPHERE_NTP_SERVER
+check_param BOSH_VSPHERE_NET_ID
+check_param BOSH_VSPHERE_VCENTER
+check_param BOSH_VSPHERE_VCENTER_USER
+check_param BOSH_VSPHERE_VCENTER_PASSWORD
+check_param BOSH_VSPHERE_VCENTER_DC
+check_param BOSH_VSPHERE_VCENTER_CLUSTER
+check_param BOSH_VSPHERE_VCENTER_RESOURCE_POOL
+check_param BOSH_VSPHERE_VCENTER_DATASTORE_PATTERN
+check_param BOSH_VSPHERE_VCENTER_UBOSH_DATASTORE_PATTERN
+check_param BAT_NETWORKING
+check_param BAT_DIRECTOR
+check_param BAT_DNS_HOST
+check_param BAT_DEPLOYMENT_SPEC
+check_param BOSH_VSPHERE_MICROBOSH_IP
+check_param BOSH_VSPHERE_VCENTER_FOLDER_PREFIX
 
 source /etc/profile.d/chruby.sh
 chruby 2.1.2
 
 cpi_release_name=bosh-vsphere-cpi
 
-BAT_STEMCELL="$PWD$BAT_STEMCELL"
-BAT_VCAP_PRIVATE_KEY="$PWD$BAT_VCAP_PRIVATE_KEY"
-BAT_DEPLOYMENT_SPEC="$PWD$BAT_DEPLOYMENT_SPEC"
+BAT_STEMCELL="${PWD}${BAT_STEMCELL}"
+BAT_VCAP_PRIVATE_KEY="${PWD}${BOSH_SSH_PRIVATE_KEY}"
+BAT_DEPLOYMENT_SPEC="${PWD}${BAT_DEPLOYMENT_SPEC}"
 
 #vsphere uses user/pass and the cdrom drive, not a reverse ssh tunnel
 eval $(ssh-agent)
-chmod go-r $BOSH_SSH_PRIVATE_KEY
-ssh-add $BOSH_SSH_PRIVATE_KEY
+chmod go-r $BAT_VCAP_PRIVATE_KEY
+ssh-add $BAT_VCAP_PRIVATE_KEY
 
 echo "using bosh CLI version..."
 bosh version
