282f13feb4d50a9eaa514f5a50707072469a61c3
Wagner Camarao <wagner@camarao.io>
2015-07-14 17:15:14 -0700

    Fix up pre deploy rogue vm check
    Signed-off-by: Caleb Miles <cmiles@pivotal.io>

--- ci/tasks/deploy.sh
+++ ci/tasks/deploy.sh
@@ -161,7 +161,10 @@ echo "deleting existing BOSH Director VM..."
 $initexe delete ${manifest_dir}/${manifest_filename}
 
 echo "verifying no BOSH deployed VM exists at target IP: $DIRECTOR_IP"
-! $(nc -vz -w10 $DIRECTOR_IP 6868)
+if [ ! $(nc -vz -w10 $DIRECTOR_IP 6868) ]; then
+  echo "aborting due to vm existing at $DIRECTOR_IP"
+  exit 1
+fi
 
 echo "deploying BOSH..."
 $initexe deploy ${manifest_dir}/${manifest_filename}
