b58499e904f52769daf474b1b97149be1a9c2c9e
Corey Innis <cinnis@pivotal.io>
2016-01-14 12:00:43 -0800

    Deletes unused spec helper (#by)

--- src/vsphere_cpi/spec/spec_helper.rb
+++ src/vsphere_cpi/spec/spec_helper.rb
@@ -8,13 +8,3 @@ Dir[Pathname(__FILE__).parent.join('support', '**/*.rb')].each { |file| require 
 class VSphereSpecConfig
   attr_accessor :logger, :uuid
 end
-
-def by(message)
-  if block_given?
-    yield
-  else
-    pending message
-  end
-end
-
-alias and_by by
