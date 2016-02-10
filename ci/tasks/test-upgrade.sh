27c669997bc17d8ce063d0ba22d017b4b35df23b
Edwin Xie <exie@pivotal.io>
2016-02-09 17:23:59 -0800

    Added time to commands in test-upgrade.sh to profile upgrader job
    [#113382709](https://www.pivotaltracker.com/story/show/113382709)

--- ci/tasks/test-upgrade.sh
+++ ci/tasks/test-upgrade.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-set -e
+set -e -x
 
 release_dir="$( cd $(dirname $0) && cd ../.. && pwd )"
 workspace_dir="$( cd ${release_dir} && cd .. && pwd )"
@@ -24,10 +24,10 @@ network1=$(env_attr "${metadata}" "network1")
 log "Using environment: \'${env_name}\'"
 export DIRECTOR_IP=$(env_attr "${metadata}" "directorIP")
 
-cp ./bosh-cpi-artifacts/*.tgz ${cpi_release_name}.tgz
-cp ./bosh-release/release.tgz bosh-release.tgz
-cp ./stemcell/stemcell.tgz stemcell.tgz
-cp $old_deployment_dir/director-manifest* .
+time cp ./bosh-cpi-artifacts/*.tgz ${cpi_release_name}.tgz
+time cp ./bosh-release/release.tgz bosh-release.tgz
+time cp ./stemcell/stemcell.tgz stemcell.tgz
+time cp $old_deployment_dir/director-manifest* .
 
 initver=$(cat bosh-init/version)
 initexe="bosh-init/bosh-init-${initver}-linux-amd64"
@@ -37,25 +37,21 @@ log "using bosh-init CLI version..."
 $initexe version
 
 log "upgrading existing BOSH Director VM..."
-$initexe deploy director-manifest.yml
-log "finished upgrading director"
+time $initexe deploy director-manifest.yml
 
-cp director-manifest* $deployment_dir
-cp -r $HOME/.bosh_init $deployment_dir
+time cp director-manifest* $deployment_dir
+time cp -r $HOME/.bosh_init $deployment_dir
 
-bosh -n target ${DIRECTOR_IP}
-bosh login ${director_username} ${director_password}
-bosh download manifest ${test_deployment_name} ${test_deployment_name}-manifest
-bosh deployment ${test_deployment_name}-manifest
+time bosh -n target ${DIRECTOR_IP}
+time bosh login ${director_username} ${director_password}
+time bosh download manifest ${test_deployment_name} ${test_deployment_name}-manifest
+time bosh deployment ${test_deployment_name}-manifest
 
 log "recreating existing BOSH Deployment..."
-bosh -n deploy --recreate
-log "finished recreating deployment"
+time bosh -n deploy --recreate
 
 log "deleting deployment..."
-bosh -n delete deployment ${test_deployment_name}
-log "finished deleting deployment"
+time bosh -n delete deployment ${test_deployment_name}
 
 log "cleaning up director..."
-bosh -n cleanup --all
-log "done cleaning up director"
+time bosh -n cleanup --all
