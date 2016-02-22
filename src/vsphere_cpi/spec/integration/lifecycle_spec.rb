0d55b9695104fc9864736e2413f31296f3d74ca7
Lyle Franklin <lfranklin@pivotal.io>
2016-02-22 15:10:05 -0800

    Add integration test for unsafe disk mode error message on delete_vm
    [#108644364](https://www.pivotaltracker.com/story/show/108644364)

--- src/vsphere_cpi/spec/integration/lifecycle_spec.rb
+++ src/vsphere_cpi/spec/integration/lifecycle_spec.rb
@@ -578,6 +578,64 @@ describe VSphereCloud::Cloud, external_cpi: false do
       end
     end
 
+    context 'when a persistent disk is attached in "nonpersistent" mode' do
+      before do
+        @vm_id = @cpi.create_vm(
+          'agent-007',
+          @stemcell_id,
+          resource_pool,
+          network_spec,
+          [],
+          {'key' => 'value'}
+        )
+        expect(@vm_id).to_not be_nil
+        expect(@cpi.has_vm?(@vm_id)).to be(true), 'Expected has_vm? to be true'
+
+        @disk_id = @cpi.create_disk(2048, {}, @vm_id)
+        expect(@disk_id).to_not be_nil
+
+        @cpi.attach_disk(@vm_id, @disk_id)
+
+        expect(@cpi.has_disk?(@disk_id)).to be(true), 'Expected has_disk? to be true'
+
+        @vm = @cpi.vm_provider.find(@vm_id)
+
+        # power off VM first to ensure new disk mode is in use
+        @vm.power_off
+
+        virtual_disk = @vm.persistent_disks.first
+        virtual_disk.backing.disk_mode = VimSdk::Vim::Vm::Device::VirtualDiskOption::DiskMode::INDEPENDENT_NONPERSISTENT
+        edit_spec = VSphereCloud::Resources::VM.create_edit_device_spec(virtual_disk)
+
+        config = VimSdk::Vim::Vm::ConfigSpec.new
+        config.device_change << edit_spec
+
+        @cpi.client.reconfig_vm(@vm.mob, config)
+        @vm.power_on
+      end
+
+      after do
+        # use lower-level client calls to bypass disk safety checks on cleanup
+        if @vm_id
+          @cpi.client.power_off_vm(@vm.mob)
+          @cpi.client.delete_vm(@vm.mob)
+        end
+
+        if @disk_id && @cpi.has_disk?(@disk_id)
+          @cpi.delete_disk(@disk_id)
+        end
+      end
+
+      it 'refuses to delete the VM' do
+        expect do
+          @cpi.delete_vm(@vm_id)
+        end.to raise_error(/The following disks are attached with non-persistent disk modes/)
+
+        expect(@cpi.has_vm?(@vm_id)).to be(true), "Expected #{@vm_id} to still exist"
+        expect(@cpi.has_disk?(@disk_id)).to be(true), 'Expected has_disk? to be true'
+      end
+    end
+
     context 'when disk is in non-accessible datastore' do
       let(:vm_cluster) { @cluster }
       let(:cpi_for_vm) do
