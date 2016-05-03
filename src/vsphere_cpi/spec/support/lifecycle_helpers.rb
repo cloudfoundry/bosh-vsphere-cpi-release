530e22fc6f186581129a86cb706b6b8f29dfbe6d
Edwin Xie <exie@pivotal.io>
2016-05-03 12:24:56 -0700

    Adds a lifecycle test for Host-local DS: Migrating disk from cluster A to B
    - it migrates the disk from a host-local datastore in cluster A to a
      host-local datastore in cluster B
    - uses two CPIs to ensure initial disk placement
    
    [#114981165](https://www.pivotaltracker.com/story/show/114981165)
    
    Signed-off-by: Felix Riegger <felix.riegger@sap.com>

--- src/vsphere_cpi/spec/support/lifecycle_helpers.rb
+++ src/vsphere_cpi/spec/support/lifecycle_helpers.rb
@@ -20,6 +20,7 @@ class LifecycleHelpers
     'BOSH_VSPHERE_CPI_SECOND_RESOURCE_POOL' => 'Please ensure you provide a name of the second resource pool within the first cluster.',
     'BOSH_VSPHERE_CPI_SECOND_CLUSTER_RESOURCE_POOL' => 'Please ensure you provide a name of a resource pool within the second cluster.',
     'BOSH_VSPHERE_CPI_SECOND_CLUSTER_DATASTORE' => 'Please ensure you provide a pattern of a second datastore attached to the second cluster.',
+    'BOSH_VSPHERE_CPI_SECOND_CLUSTER_LOCAL_DATASTORE' => 'Please ensure you provide a pattern of a local datastore attached to the second cluster.',
     'BOSH_VSPHERE_CPI_PERSISTENT_DATASTORE_PATTERN' => 'Please ensure you provide a pattern of a persistent datastore attached to all hosts in all provided clusters.',
     'BOSH_VSPHERE_CPI_DISK_PATH' => 'Please ensure you provide a disk path.',
     'BOSH_VSPHERE_CPI_TEMPLATE_FOLDER' => 'Please ensure you provide a template folder.',
