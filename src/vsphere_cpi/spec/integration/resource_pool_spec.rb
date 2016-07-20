84466dcea1f9a7265b101757f9a83947dca28f77
Forrest Sill <fsill@pivotal.io>
2016-07-20 10:56:51 -0700

    Clarify what is being tested in resource_pool_spec
    - Some of the setup relied on decisions made in the top-level spec helper,
      we made these decisions more explicit directly in the test
    
    [#121876887](https://www.pivotaltracker.com/story/show/121876887)
    
    Signed-off-by: Lyle Franklin <lfranklin@pivotal.io>

--- src/vsphere_cpi/spec/integration/resource_pool_spec.rb
+++ src/vsphere_cpi/spec/integration/resource_pool_spec.rb
@@ -1,6 +1,6 @@
 require 'integration/spec_helper'
 
-context 'given a cpi that is configured' do
+context 'given a CPI configured with vSphere resource pools' do
   let(:network_spec) do
     {
       'static' => {
@@ -22,91 +22,94 @@ context 'given a cpi that is configured' do
     }
   end
 
-  context 'without specifying the vSphere resource pool' do
-    let(:second_cpi) do
-      options = cpi_options(
-      clusters: [@cluster]
-      )
-      VSphereCloud::Cloud.new(options)
-    end
+  let(:resource_pool_cpi) do
+    options = cpi_options(
+      clusters: [{ @cluster => {'resource_pool' => @resource_pool_name} }],
+    )
+    VSphereCloud::Cloud.new(options)
+  end
 
-    it 'can still find a vm created by a cpi configured with a vSphere resource pool' do
-      begin
-        vm_id = @cpi.create_vm(
+  it 'should exercise the vm lifecycle' do
+    vm_lifecycle(resource_pool_cpi, [], resource_pool, network_spec, @stemcell_id)
+  end
+
+  context 'when a VM already exists in a vSphere resource pool' do
+    vm_id = nil
+
+    before do
+      vm_id = resource_pool_cpi.create_vm(
         'agent-007',
         @stemcell_id,
         resource_pool,
         network_spec,
         [],
         {}
-        )
+      )
+      expect(vm_id).to_not be_nil
+    end
 
-        expect(vm_id).to_not be_nil
+    after do
+      delete_vm(resource_pool_cpi, vm_id)
+    end
+
+    context 'and a second CPI is not configured with the resource pool' do
+      let(:second_cpi) do
+        options = cpi_options(
+          clusters: [@cluster]
+        )
+        VSphereCloud::Cloud.new(options)
+      end
+
+      it 'can still find a vm created by a cpi configured with a vSphere resource pool' do
         expect(second_cpi.has_vm?(vm_id)).to be(true)
-      ensure
-        delete_vm(@cpi, vm_id)
       end
-    end
 
-    it 'can still find the stemcell created by a cpi configured with a vSphere resource pool' do
-      expect(second_cpi.stemcell_vm(@stemcell_id)).to_not be_nil
-    end
-
-    it 'should exercise the vm lifecycle' do
-      vm_lifecycle(second_cpi, [], resource_pool, network_spec, @stemcell_id)
-    end
-  end
-
-  context 'when resource_pool is set to the first cluster' do
-    it 'places vm in first cluster when vsphere resource pool is not provided' do
-      begin
-        resource_pool['datacenters'] = [{'name' => @datacenter_name, 'clusters' => [{@cluster => {}}]}]
-        vm_id = @cpi.create_vm(
-          'agent-007',
-          @stemcell_id,
-          resource_pool,
-          network_spec
-        )
-
-        vm = @cpi.vm_provider.find(vm_id)
-        expect(vm.cluster).to eq(@cluster)
-        default_resource_pool = @resource_pool_name
-        expect(vm.resource_pool).to_not eq(default_resource_pool)
-      ensure
-        delete_vm(@cpi, vm_id)
-      end
-    end
-
-    it 'places vm in the specified resource pool when vsphere resource pool is provided' do
-      begin
-        resource_pool['datacenters'] = [{'name' => @datacenter_name, 'clusters' => [{@cluster => {'resource_pool' => @second_resource_pool_within_cluster}}]}]
-        vm_id = @cpi.create_vm(
-          'agent-007',
-          @stemcell_id,
-          resource_pool,
-          network_spec
-        )
-
-        vm = @cpi.vm_provider.find(vm_id)
-        expect(vm.cluster).to eq(@cluster)
-        expect(vm.resource_pool).to eq(@second_resource_pool_within_cluster)
-      ensure
-        delete_vm(@cpi, vm_id)
+      it 'can still find the stemcell created by a cpi configured with a vSphere resource pool' do
+        expect(second_cpi.stemcell_vm(@stemcell_id)).to_not be_nil
       end
     end
   end
 
-  context 'when resource_pool is set to the second cluster' do
-    it 'places vm in second cluster' do
+  context 'when vm_type.resource_pool is set to a cluster' do
+    let(:second_cluster_cpi) do
       options = cpi_options(
         datastore_pattern: @second_cluster_datastore,
         persistent_datastore_pattern: @second_cluster_datastore
       )
-      second_cluster_cpi = VSphereCloud::Cloud.new(options)
+      VSphereCloud::Cloud.new(options)
+    end
+
+    before do
       resource_pool['datacenters'] = [{'name' => @datacenter_name, 'clusters' => [{@second_cluster => {}}]}]
+    end
+
+    it 'places vm in provided cluster' do
       vm_lifecycle(second_cluster_cpi, [], resource_pool, network_spec, @stemcell_id) do |vm_id|
         vm = second_cluster_cpi.vm_provider.find(vm_id)
         expect(vm.cluster).to eq(@second_cluster)
+      end
+    end
+  end
+
+  context 'when vm_type.resource_pool is set to a resource pool within the cluster' do
+    before do
+      resource_pool['datacenters'] = [{'name' => @datacenter_name, 'clusters' => [{@cluster => {'resource_pool' => @second_resource_pool_within_cluster}}]}]
+    end
+
+    it 'places vm in the specified resource pool, overriding the globally configured resource pool' do
+      begin
+        vm_id = resource_pool_cpi.create_vm(
+          'agent-007',
+          @stemcell_id,
+          resource_pool,
+          network_spec
+        )
+
+        vm = resource_pool_cpi.vm_provider.find(vm_id)
+        expect(vm.cluster).to eq(@cluster)
+        expect(vm.resource_pool).to eq(@second_resource_pool_within_cluster)
+      ensure
+        delete_vm(resource_pool_cpi, vm_id)
       end
     end
   end
