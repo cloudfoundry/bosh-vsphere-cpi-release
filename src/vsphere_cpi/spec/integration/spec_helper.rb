1cffd80a772f72307d036d5478a53f0d149cda65
Brian Cunnie <bcunnie@pivotal.io>
2017-03-27 16:15:11 -0700

    Fix issue where suite_cpi is shadowed by nil
    ruby is fun
    
    Signed-off-by: Zak Auerbach <zauerbach@pivotal.io>

--- src/vsphere_cpi/spec/integration/spec_helper.rb
+++ src/vsphere_cpi/spec/integration/spec_helper.rb
@@ -7,17 +7,18 @@ RSpec.configure do |rspec_config|
   # before(:suite) and before(:all) seem to run in different contexts
   # so we can't assign directly to @stemcell; using a closure instead
   stemcell_id = nil
+  suite_cpi = nil
 
   rspec_config.before(:suite) do
     setup_global_config
     fetch_global_properties
 
-    @suite_cpi = VSphereCloud::Cloud.new(cpi_options)
+    suite_cpi = VSphereCloud::Cloud.new(cpi_options)
 
     Dir.mktmpdir do |temp_dir|
       stemcell_image = stemcell_image(@stemcell_path, temp_dir)
       # stemcell uploads are slow on local vSphere, only upload once
-      stemcell_id = @suite_cpi.create_stemcell(stemcell_image, nil)
+      stemcell_id = suite_cpi.create_stemcell(stemcell_image, nil)
     end
   end
 
@@ -34,6 +35,6 @@ RSpec.configure do |rspec_config|
   end
 
   rspec_config.after(:suite) do
-    delete_stemcell(@suite_cpi, stemcell_id)
+    delete_stemcell(suite_cpi, stemcell_id)
   end
 end
