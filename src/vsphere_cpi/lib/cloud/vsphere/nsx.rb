6aa666bdfab49fa84feb3cdbd1d11c94744b3108
Sam Bryant <sam.bryant@fil.com>
2017-07-10 11:33:31 +0100

    retry adding vms to security groups when receiving optimisticlock exceptions

--- src/vsphere_cpi/lib/cloud/vsphere/nsx.rb
+++ src/vsphere_cpi/lib/cloud/vsphere/nsx.rb
@@ -21,32 +21,31 @@ module VSphereCloud
     end
 
     def add_vm_to_security_group(security_group_name, vm_id)
-      sg_id = find_or_create_security_group(security_group_name)
-
-      @retryer.try(MAX_TRIES) do |i|
-        if i == 0
+      i = 0
+      Bosh::Retryable.new(tries: MAX_TRIES,
+                          sleep: ->(try_count, retry_exception) { 0.5 },
+                          on: [Exception],
+                          matching: %r{(?:javax.persistence.OptimisticLockException|<errorCode>300<\/errorCode>)}).retryer do
+        sg_id = find_or_create_security_group(security_group_name)
+        if i.zero?
           @logger.debug("Adding VM '#{vm_id}' to Security Group '#{security_group_name}'...")
         else
           @logger.warn("Retrying adding VM '#{vm_id}' to Security Group '#{security_group_name}', #{i} attempts so far...")
         end
 
+        i += 1
+
         response = @http_client.put("https://#{@nsx_url}/api/2.0/services/securitygroup/#{sg_id}/members/#{vm_id}", nil)
-        unless response.status.between?(200, 299) || vm_belongs_to_security_group?(response.body)
-          unless is_attach_error_retryable?(response.body)
-            raise "Failed to add VM to Security Group with unknown NSX error: '#{response.body}'"
-          end
 
-          err = "Failed to locate VM with VM ID '#{vm_id}' via NSX API: '#{response.body}'"
-          @logger.warn(err)
-          [nil, err]
-        else
-          [response, nil]
+        return true if vm_belongs_to_security_group?(response.body)
+
+        unless response.status.between?(200, 299)
+          raise "Failed to add VM to Security Group with unknown NSX error: '#{response.body}'"
         end
+
+        @logger.debug("Successfully added VM '#{vm_id}' to Security Group '#{security_group_name}'.")
+        response.status.between?(200, 299)
       end
-
-      @logger.debug("Successfully added VM '#{vm_id}' to Security Group '#{security_group_name}'.")
-
-      true
     end
 
     # Note: this method should only be used for cleanup in integration tests
@@ -341,14 +340,6 @@ module VSphereCloud
       end
 
       existing_pool_element.to_xml
-    end
-
-    def is_attach_error_retryable?(xml_content)
-      error_document = Oga.parse_xml(xml_content)
-      error_code_element = error_document.xpath('error/errorCode')
-
-      vm_not_found = (!error_code_element.empty? && error_code_element.text == '300')
-      vm_not_found ? true : false
     end
 
     def vm_belongs_to_security_group?(xml_content)
