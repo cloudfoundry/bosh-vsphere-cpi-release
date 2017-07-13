edcbdbb326c8cfa0d55f19ee7df168800055a361
Chris Dutra <cdutra@pivotal.io>
2017-07-13 09:25:30 -0700

    gen_all.sh fail if version is not found

--- scripts/pyvmomi_to_ruby/README.md
+++ scripts/pyvmomi_to_ruby/README.md
@@ -1,10 +1,13 @@
+# pyvmomi_to_ruby
+
+This tool generates ruby_vim_sdk types from the pyvmomi sdk.
+
 [![Build Status](https://travis-ci.org/mmb/pyvmomi_to_ruby.svg?branch=master)](https://travis-ci.org/mmb/pyvmomi_to_ruby)
 
 Convert [pyvmomi](https://github.com/vmware/pyvmomi) CoreTypes.py and
 ServerObjects.py files to ruby versions usable by [ruby_vim_sdk](https://github.com/cloudfoundry/bosh/tree/master/bosh_vsphere_cpi/lib/ruby_vim_sdk).
 
 Usage:
-
 ```
 ./gen_all.sh "version_tag"
 ```
@@ -12,4 +15,10 @@ Usage:
 Version used by current BOSH vSphere CPI:
 ```
 ./gen_all.sh "v5.5.0.2014.1.1"
+```
+
+## Running tests
+
+```
+python -m test
 ```
--- scripts/pyvmomi_to_ruby/gen_all.sh
+++ scripts/pyvmomi_to_ruby/gen_all.sh
@@ -5,12 +5,13 @@ ruby_vim_sdk_path="${SRC_DIR}/lib/ruby_vim_sdk"
 version_tag=$1
 : ${version_tag:=?}
 
-tag="$( curl -L https://api.github.com/repos/vmware/pyvmomi/tags | jq '.[] | select(.name=="${version_tag}")' )"
+tag="$( curl -L https://api.github.com/repos/vmware/pyvmomi/tags | jq --arg version "${version_tag}" '.[] | select(.name==$version) | .name' )"
 if [[ -z "${tag}" ]]; then
-  echo "Version tag '${version_tag}'  not found!"
+  echo "Version tag '${version_tag}' not found!"
   exit 1
 fi
 
+echo "Fetching version '${tag}' ...'"
 vmware_pyvmimu_url="https://raw.githubusercontent.com/vmware/pyvmomi"
 files_to_download="{ServerObjects.py,CoreTypes.py}"
 
