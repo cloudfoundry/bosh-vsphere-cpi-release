8498bc68106059975a84f85d49fbdb83f9878b6e
Chris Dutra <cdutra@pivotal.io>
2017-07-12 21:16:19 -0700

    Check if version tag exists on pyvmomi repo

--- scripts/pyvmomi_to_ruby/gen_all.sh
+++ scripts/pyvmomi_to_ruby/gen_all.sh
@@ -4,6 +4,13 @@ ruby_vim_sdk_path="${SRC_DIR}/lib/ruby_vim_sdk"
 
 version_tag=$1
 : ${version_tag:=?}
+
+tag="$( curl -L https://api.github.com/repos/vmware/pyvmomi/tags | jq '.[] | select(.name=="${version_tag}")' )"
+if [[ -z "${tag}" ]]; then
+  echo "Version tag '${version_tag}'  not found!"
+  exit 1
+fi
+
 vmware_pyvmimu_url="https://raw.githubusercontent.com/vmware/pyvmomi"
 files_to_download="{ServerObjects.py,CoreTypes.py}"
 
