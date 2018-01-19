26b777b6a6c748746ebfb1b461406f5f6eaa577e
Tushar Aggarwal <taggarwal@vmware.com>
2018-01-18 16:33:34 -0800

    Incorporate pool resource to fetch nimbus env
    - Change http proxy port to 80
    - Add lock and extension for nimbus env

---
+++ ci/tasks/extend-lease.sh
@@ -0,0 +1,26 @@
+#! /bin/bash
+
+# Set remote from DBCUSER and DBCHOST
+DBCUSER=${DBCUSER:-$(id -un)}
+if [ -z "$DBCHOST" ]; then
+  echo DBCHOST must be set to the unqualified hostname of your DBC host 1>&2
+  exit 1
+fi
+remote=$DBCUSER@$DBCHOST.eng.vmware.com
+
+echo "$DBC_SSH_KEY" > dbc_ssh_key
+chmod 400 dbc_ssh_key
+
+metadata_file=nimbus-environments-6.5/metadata
+
+source "$metadata_file"
+
+if [ -z "$VCPI_NIMBUS_LAUNCH_NAME" ]; then
+  echo Unable to read Nimbus testbed launch name from "$metadata_file" 1>&2
+  exit 1
+fi
+
+echo Extending Nimbus testbed "$VCPI_NIMBUS_LAUNCH_NAME" ... 1>&2
+
+ssh -i dbc_ssh_key -o StrictHostKeyChecking=no $remote \
+  /mts/git/bin/nimbus-ctl --lease 1 --testbed extend-lease "$VCPI_NIMBUS_LAUNCH_NAME"
--- ci/tasks/run-lifecycle.sh
+++ ci/tasks/run-lifecycle.sh
@@ -4,34 +4,11 @@ set -e
 
 source bosh-cpi-src/.envrc
 
-# Spawn the test environment on nimbus
-pushd vcpi-nimbus
-  echo "$DBC_SSH_KEY" > ./dbc_ssh_key
-  chmod 400 dbc_ssh_key
-  ./launch -e deployment.sh -s 'ssh -i dbc_ssh_key -o StrictHostKeyChecking=no'
-  source environment.sh
-  source deployment.sh
-popd
-
-# Sleep for 15 minutes to allow the system to start collecting statistics.
-# PerformanceManager provides wrong statistics if this is not there and most
-# tests will fail.
-#
-# DO NOT REMOVE OR CHANGE WITHOUT THOROUGH INVESTIGATION.
-#
-# According to the vSphere documentation for PerformanceManager:
-#   VirtualCenter Server 2.5 (and subsequent vCenter Server) systems initially
-#   collect statistics data 10 minutes after system startup
-# Sleep for 16 minutes (960 seconds) to allow for statistics collection to
-# commence and for one round (5 minutes) of statistics to be collected. Allow
-# one more minutes as a buffer.
-sleep 960
-
+source nimbus-environments-6.5/metadata
+export HTTP_PROXY="http://$BOSH_VSPHERE_JUMPER_HOST:80"
+export HTTPS_PROXY="http://$BOSH_VSPHERE_JUMPER_HOST:80"
 stemcell_dir="$( cd stemcell && pwd )"
-
 export BOSH_VSPHERE_STEMCELL=${stemcell_dir}/stemcell.tgz
-export HTTP_PROXY="http://$BOSH_VSPHERE_JUMPER_HOST:3128"
-export HTTPS_PROXY="http://$BOSH_VSPHERE_JUMPER_HOST:3128"
 
 : ${RSPEC_FLAGS:=""}
 : ${BOSH_VSPHERE_STEMCELL:=""}
