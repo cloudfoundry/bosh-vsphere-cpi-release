fbdc023d82d3e9ee87a40ca5b162bd5128f1ffb5
Neha Jain <jneha@vmware.com>
2018-02-21 10:32:02 -0800

    Move cluster related integration spec into correct file

--- src/vsphere_cpi/spec/integration/cluster_cloud_properties_spec.rb
+++ src/vsphere_cpi/spec/integration/cluster_cloud_properties_spec.rb
@@ -165,4 +165,92 @@ describe 'cloud_properties related to clusters' do
       end
     end
   end
+  context 'when disk_pool specifies a datastore accessible from cluster defined in vm_type' do
+    before (:all) do
+      @datastore_pattern = fetch_and_verify_datastore('BOSH_VSPHERE_CPI_SINGLE_LOCAL_DATASTORE_PATTERN', @cluster_name)
+      #cluster which has a disjoint datastore (not shared with any other cluster)
+      @disjoint_cluster = fetch_and_verify_cluster('BOSH_VSPHERE_CPI_SECOND_CLUSTER')
+      @disjoint_datastore = fetch_and_verify_datastore('BOSH_VSPHERE_CPI_SECOND_CLUSTER_DATASTORE', @disjoint_cluster)
+    end
+    let(:vm_type) do
+      {
+        'ram' => 512,
+        'disk' => 2048,
+        'cpu' => 1,
+        'datacenters' => [
+          {
+            'name' => @datacenter_name,
+            'clusters' => [
+              {
+                @disjoint_cluster => {}
+              }
+            ]
+          }
+        ]
+      }
+    end
+    let(:cpi) do
+      options = cpi_options(
+        'datacenters' => [
+          {
+            'name' => @datacenter_name,
+            'datastore_pattern' => @datastore_pattern,
+            'persistent_datastore_pattern' => @datastore_pattern,
+            'clusters' => [
+              {
+                @cluster_name => {}
+              },
+            ]
+          }
+        ]
+      )
+      VSphereCloud::Cloud.new(options)
+    end
+
+    let(:disjoint_cluster_cpi) do
+      options = cpi_options(
+        'datacenters' => [
+          {
+            'name' => @datacenter_name,
+            'datastore_pattern' => @datastore_pattern,
+            'persistent_datastore_pattern' => @datastore_pattern,
+            'clusters' => [
+              {
+                @disjoint_cluster => {}
+              },
+            ]
+          }
+        ]
+      )
+      VSphereCloud::Cloud.new(options)
+    end
+
+    let(:disk_pool) { { 'datastores' => [@disjoint_datastore] } }
+
+    it 'should place disk into datastore that belongs to the cluster defined in cloud config' do
+      begin
+        vm_id = cpi.create_vm(
+          'agent-007',
+          @stemcell_id,
+          vm_type,
+          get_network_spec,
+          [],
+          {}
+        )
+        expect(vm_id).to_not be_nil
+        vm = cpi.vm_provider.find(vm_id)
+        expect(vm.cluster).to eq(@disjoint_cluster)
+        expect(cpi.has_vm?(vm_id)).to be(true)
+
+        disk_id = cpi.create_disk(2048, disk_pool, vm_id)
+        expect(disk_id).to_not be_nil
+        expect(disjoint_cluster_cpi.has_disk?(disk_id)).to be(true)
+        disk = disjoint_cluster_cpi.datacenter.find_disk(VSphereCloud::DirectorDiskCID.new(disk_id))
+        expect(disk.datastore.name).to eq(@disjoint_datastore)
+      ensure
+        delete_vm(cpi, vm_id)
+        delete_disk(disjoint_cluster_cpi, disk_id)
+      end
+    end
+  end
 end
