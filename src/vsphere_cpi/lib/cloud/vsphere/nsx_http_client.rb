132239cbe69f21fd9209e2b70df4c08809799f70
Tushar Aggarwal <taggarwal@vmware.com>
2018-02-22 16:17:01 -0800

    Add support for nsxv testing
    - Correct resource name in lifecycle-6.x jobs to remove the environment
    post failure.

--- src/vsphere_cpi/lib/cloud/vsphere/nsx_http_client.rb
+++ src/vsphere_cpi/lib/cloud/vsphere/nsx_http_client.rb
@@ -1,12 +1,19 @@
 module VSphereCloud
   class NsxHttpClient < BaseHttpClient
     def initialize(user, password, http_log = nil)
-      super(
-        http_log: http_log,
-        trusted_ca_file: ENV['BOSH_NSX_CA_CERT_FILE'],
-        ca_cert_manifest_key: 'vcenter.nsx.ca_cert',
-        skip_ssl_verify: false
-      )
+      unless ENV['NSXT_SKIP_SSL_VERIFY'] then
+        super(
+          http_log: http_log,
+          trusted_ca_file: ENV['BOSH_NSX_CA_CERT_FILE'],
+          ca_cert_manifest_key: 'vcenter.nsx.ca_cert',
+          skip_ssl_verify: false
+        )
+      else
+        super(
+          http_log: http_log,
+          skip_ssl_verify: true
+        )
+      end
 
       @backing_client.set_auth(nil, user, password)
       # NSX returns 403, not 401, so we need force basic auth from the get-go
@@ -14,4 +21,3 @@ module VSphereCloud
     end
   end
 end
-
