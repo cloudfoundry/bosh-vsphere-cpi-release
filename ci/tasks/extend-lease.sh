821bef398f9b2970159525afff33234fd7149b2d
Kaiting Chen <kaitingc@vmware.com>
2018-03-15 11:27:00 -0700

    Respect Nimbus launch user

--- ci/tasks/extend-lease.sh
+++ ci/tasks/extend-lease.sh
@@ -18,13 +18,21 @@ while IFS= read -r text; do
   eval "${text#'# '}"
 done < <(sed -e "1,/^$SENTINEL/d" $metadata_file | grep '^# VCPI_NIMBUS')
 
-
 if [ -z "$VCPI_NIMBUS_LAUNCH_NAME" ]; then
   echo Unable to read Nimbus testbed launch name from "$metadata_file" 1>&2
   exit 1
 fi
 
-echo Extending Nimbus testbed "$VCPI_NIMBUS_LAUNCH_NAME" ... 1>&2
+if [ -z "$VCPI_NIMBUS_LAUNCH_USER" ]; then
+  echo Unable to read Nimbus testbed launch user from "$metadata_file" 1>&2
+  echo "This step may fail if the testbed wasn't launched as $DBCUSER" 1>&2
+  VCPI_NIMBUS_LAUNCH_USER="$DBCUSER"
+fi
+
+echo Extending lease on Nimbus testbed "$VCPI_NIMBUS_LAUNCH_NAME" ... 1>&2
 
 ssh -i dbc_ssh_key -o StrictHostKeyChecking=no $remote \
-  /mts/git/bin/nimbus-ctl --lease 1 --testbed extend-lease "$VCPI_NIMBUS_LAUNCH_NAME"
+  USER="$VCPI_NIMBUS_LAUNCH_USER" \
+  /mts/git/bin/nimbus-ctl \
+  --testbed --lease 1 extend-lease \
+  "$VCPI_NIMBUS_LAUNCH_NAME"
