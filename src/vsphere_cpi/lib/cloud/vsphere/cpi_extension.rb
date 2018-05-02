2bf0c5efac4f29cc30adcabc933abd67133c10eb
Tushar Aggarwal <taggarwal@vmware.com>
2018-05-02 16:23:21 -0700

    Enable CPI extensions
    - Add unit & integration test
    - Does not add managed by info if extension is not present.
    - Tries to create default extension on every create stemcell call.
    
    [Delivers #157263518](https://www.pivotaltracker.com/story/show/157263518)

---
+++ src/vsphere_cpi/lib/cloud/vsphere/cpi_extension.rb
@@ -0,0 +1,69 @@
+module VSphereCloud
+  module VCPIExtension
+    include Logger
+    DEFAULT_VSPHERE_CPI_EXTENSION_KEY = "sddc.cpi.extension".freeze
+    DEFAULT_VSPHERE_CPI_EXTENSION_LABEL = "BOSH vSphere CPI".freeze
+    DEFAULT_VSPHERE_CPI_EXTENSION_SUMMARY = "BOSH vSphere CPI Extension".freeze
+    DEFAULT_VSPHERE_CPI_EXTENSION_VERSION = "1.0.0".freeze
+    DEFAULT_VSPHERE_MANAGED_BY_INFO_DESC = "The lifecycle of this resource is managed by BOSH".freeze
+    DEFAULT_VSPHERE_MANAGED_BY_INFO_RESOURCE = "cpi-managed-resource".freeze
+
+    def create_cpi_extension(client)
+      @vc_client = client
+
+      return if extension_is_registered?
+
+      logger.debug("Creating the vSphere CPI extension ...")
+      cpi_extension = build_extension
+      register_extension(cpi_extension)
+    rescue => e
+      logger.error("Failed to add extension with message #{e.inspect}")
+    end
+
+    private
+
+    def vc_client
+      @vc_client
+    end
+
+    def build_extension(key: DEFAULT_VSPHERE_CPI_EXTENSION_KEY,
+      version: DEFAULT_VSPHERE_CPI_EXTENSION_VERSION,
+      label: DEFAULT_VSPHERE_CPI_EXTENSION_LABEL,
+      summary: DEFAULT_VSPHERE_CPI_EXTENSION_SUMMARY,
+      managed_by_info_desc: DEFAULT_VSPHERE_MANAGED_BY_INFO_DESC,
+      managed_by_info_resource: DEFAULT_VSPHERE_MANAGED_BY_INFO_RESOURCE, **option
+    )
+      cpi_extension = VimSdk::Vim::Extension.new
+      cpi_extension.key = key
+      cpi_extension.version = version
+      cpi_extension.last_heartbeat_time = option[:last_heartbeat] || Time.now.iso8601
+
+      cpi_extension_desc = VimSdk::Vim::Description.new
+      cpi_extension_desc.label = label
+      cpi_extension_desc.summary = summary
+      cpi_extension.description = cpi_extension_desc
+
+      managed_by_info = VimSdk::Vim::Ext::ManagedEntityInfo.new
+      managed_by_info.description = managed_by_info_desc
+      managed_by_info.type = managed_by_info_resource
+      cpi_extension.managed_entity_info = [managed_by_info]
+
+      cpi_extension
+    end
+
+    def register_extension(extension)
+      ext_mgr = vc_client.service_content.extension_manager
+      ext_mgr.register_extension(extension)
+    end
+
+    def extension_is_registered?(key: DEFAULT_VSPHERE_CPI_EXTENSION_KEY)
+      ext_mgr = vc_client.service_content.extension_manager
+      cpi_extension = ext_mgr.find_extension(key)
+      logger.debug("Extension already exists")
+      return !cpi_extension.nil?
+    end
+
+    module_function :create_cpi_extension, :build_extension,
+      :register_extension, :extension_is_registered?, :vc_client
+  end
+end
---
+++ src/vsphere_cpi/spec/unit/cloud/vsphere/cpi_extension_spec.rb
@@ -0,0 +1,49 @@
+require 'spec_helper'
+
+module VSphereCloud
+  describe VCPIExtension, fake_logger: true do
+    subject { described_class }
+
+    let(:ext_mgr) { instance_double('VimSdk::Vim::ExtensionManager') }
+    let(:client) { instance_double('VSphereCloud::VCenterClient', :logout => nil) }
+    before do
+      allow(subject).to receive(:vc_client).and_return(client)
+    end
+
+    describe '#extension_is_registered?' do
+      it 'returns true when extension is registered with VC' do
+        expect(client).to receive_message_chain(:service_content, :extension_manager).and_return(ext_mgr)
+        expect(ext_mgr).to receive(:find_extension).with(described_class::DEFAULT_VSPHERE_CPI_EXTENSION_KEY).and_return('Extension')
+        expect(subject.send(:extension_is_registered?)).to eq(true)
+      end
+
+      it 'returns false when extension is not registered with VC' do
+        expect(client).to receive_message_chain(:service_content, :extension_manager).and_return(ext_mgr)
+        expect(ext_mgr).to receive(:find_extension).with(described_class::DEFAULT_VSPHERE_CPI_EXTENSION_KEY).and_return(nil)
+        expect(subject.send(:extension_is_registered?)).to eq(false)
+      end
+    end
+
+    describe '#build_extension' do
+      it 'builds extension with specified key and version' do
+        extension = subject.send(:build_extension, key: 'Foo', version: 'Bar')
+        expect(extension.key). to eq('Foo')
+        expect(extension.version).to eq('Bar')
+        expect(extension.description.label).to eq(subject::DEFAULT_VSPHERE_CPI_EXTENSION_LABEL)
+      end
+    end
+
+    describe '#create_cpi_extension' do
+      it 'creates an extension if it does not exist' do
+        expect(subject).to receive(:extension_is_registered?).and_return(false)
+        expect(subject).to receive(:register_extension).with(anything).and_return(nil)
+        subject.create_cpi_extension(client)
+      end
+
+      it 'rescues all errors and suppresses them' do
+        expect(subject).to receive(:extension_is_registered?).and_raise('boom')
+        subject.create_cpi_extension(client)
+      end
+    end
+  end
+end
