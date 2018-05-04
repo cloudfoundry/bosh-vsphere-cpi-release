458e2de4b26bd756528900ff66fc79c7650eed30
Mikhail Vyshegorodtsev <mvyshegorodtse@vmware.com>
2018-05-04 13:05:46 -0700

    add missing package compilation

--- ci/tasks/cpi-networking-release-and-publish.sh
+++ ci/tasks/cpi-networking-release-and-publish.sh
@@ -3,6 +3,17 @@
 
 mkdir  bosh-vsphere-cpi-networking-release
 
+install_iso9660wrap() {
+  pushd bosh-cpi-src
+    pushd src/iso9660wrap
+      go build ./...
+      export PATH="$PATH:$PWD"
+    popd
+  popd
+}
+
+install_iso9660wrap
+
 pushd bosh-cpi-src
     bosh create-release --tarball bosh-vsphere-cpi-networking-release.tgz --force
 popd
