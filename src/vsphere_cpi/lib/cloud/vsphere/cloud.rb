18c5143cd672fea6fbe31d935ee8fc231dfd1287
Harsh Patel <harshpatel081296@gmail.com>
2018-08-27 11:49:35 -0700

    Add filtering for gpus and creation of vm with gpus
    - vm_selection_pipeline supports filtering of hosts based on gpu
    - vm_creator provides for addition of gpu device spec to vm clone spec

--- src/vsphere_cpi/lib/cloud/vsphere/cloud.rb
+++ src/vsphere_cpi/lib/cloud/vsphere/cloud.rb
@@ -387,9 +387,11 @@ module VSphereCloud
 
     def delete_vm(vm_cid)
       with_thread_name("delete_vm(#{vm_cid})") do
-        logger.info("Deleting vm: #{vm_cid}")
 
+        logger.info("Deleting vm: #{vm_cid}")
         vm = vm_provider.find(vm_cid)
+        logger.info("Deleting vm with mob: #{vm.mob} ")
+
         vm_ip = vm.mob.guest&.ip_address
 
         # find vm_groups vm is part of before deleting it
@@ -657,6 +659,7 @@ module VSphereCloud
     def clone_vm(vm, name, folder, resource_pool, options={})
       relocation_spec = Vim::Vm::RelocateSpec.new
       relocation_spec.datastore = options[:datastore] if options[:datastore]
+      relocation_spec.host = options[:host].mob unless options[:host].nil?
       if options[:linked]
         relocation_spec.disk_move_type = Vim::Vm::RelocateSpec::DiskMoveOptions::CREATE_NEW_CHILD_DISK_BACKING
       end
