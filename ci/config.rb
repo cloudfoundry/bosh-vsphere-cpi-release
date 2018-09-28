afe6a5efa9e7d52f1a6771f5d9828e246a5a8202
Neha Jain <jneha@vmware.com>
2018-09-28 14:20:46 -0700

    Update ci to include 6.7 with nsx-t23, remove 6.0, 6.5 and 6.5 nsxv

--- ci/config.rb
+++ ci/config.rb
@@ -1,38 +1,8 @@
-$pipeline.pool('6.0') do |pool|
-  pool.params = {
-    RSPEC_FLAGS: [
-      '--tag ~nsx_vsphere',
-      '--tag ~nsxt_2',
-      '--tag ~nsxt_21',
-    ].join(' '),
-    NSXT_SKIP_SSL_VERIFY: "true"
-  }
-end
-
 $pipeline.pool('6.0-NSXV') do |pool|
   pool.params = {
     RSPEC_FLAGS: [
-      '--tag nsx_vsphere',
-    ].join(' '),
-    NSXT_SKIP_SSL_VERIFY: "true"
-  }
-end
-
-$pipeline.pool('6.5') do |pool|
-  pool.params = {
-    RSPEC_FLAGS: [
-      '--tag ~disk_migration',
-      '--tag ~nsx_vsphere',
+      '--tag ~nsxt_2',
       '--tag ~nsxt_21',
-    ].join(' '),
-    NSXT_SKIP_SSL_VERIFY: "true"
-  }
-end
-
-$pipeline.pool('6.5-NSXV') do |pool|
-  pool.params = {
-    RSPEC_FLAGS: [
-      '--tag nsx_vsphere',
     ].join(' '),
     NSXT_SKIP_SSL_VERIFY: "true"
   }
@@ -41,7 +11,8 @@ end
 $pipeline.pool('6.5-NSXT21') do |pool|
   pool.params = {
     RSPEC_FLAGS: [
-      '--tag nsxt_21',
+      '--tag ~disk_migration',
+      '--tag ~nsx_vsphere',
     ].join(' '),
     NSXT_SKIP_SSL_VERIFY: "true"
   }
@@ -50,8 +21,7 @@ end
 $pipeline.pool('6.5-NSXT22') do |pool|
   pool.params = {
     RSPEC_FLAGS: [
-      '--tag nsx_transformers',
-      '--tag nsxt_21'
+      '--tag nsx_transformers'
     ].join(' '),
     NSXT_SKIP_SSL_VERIFY: "true"
   }
@@ -60,9 +30,17 @@ end
 $pipeline.pool('6.7-NSXT22') do |pool|
   pool.params = {
     RSPEC_FLAGS: [
-      '--tag ~disk_migration',
       '--tag ~nsx_vsphere',
       '--tag ~host_maintenance',
+    ].join(' '),
+    NSXT_SKIP_SSL_VERIFY: "true"
+  }
+end
+
+$pipeline.pool('6.7-NSXT23') do |pool|
+  pool.params = {
+    RSPEC_FLAGS: [
+      '--tag nsx_transformers'
     ].join(' '),
     NSXT_SKIP_SSL_VERIFY: "true"
   }
