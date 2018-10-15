ed9b6f8493090e900e7932dbb44c3b048f9e6f7e
Neha Jain <jneha@vmware.com>
2018-10-15 13:59:31 -0700

    Add method to fetch compatible datastores for given policy

--- src/vsphere_cpi/lib/cloud/vsphere/pbm.rb
+++ src/vsphere_cpi/lib/cloud/vsphere/pbm.rb
@@ -16,19 +16,24 @@ module VSphereCloud
       @service_content = pbm_instance.retrieve_content
     end
 
-    # can be encryption_profile if we want to actually find an encryption profile
     # TODO based on requirement update this
-    def get_profile(name)
+    def get_profile(profile_name='VM Encryption Policy')
       profile_mgr = @service_content.profile_manager
       resource_type = VimSdk::Pbm::Profile::ResourceType.new
       resource_type.resource_type = 'STORAGE'
       profile_ids = profile_mgr.query_profile(resource_type)
       profiles = profile_mgr.retrieve_content(profile_ids)
-      profile = profiles.select{|p| p.name == 'VM Encryption Policy'}.first
+      profile = profiles.select{|p| p.name == profile_name}.first
       profile_id = profile.profile_id
-      profile_id#find profile matching the name and return that
+      profile_id
     end
 
+    def get_compatible_datastores(profile_id, datastores= [])
+      placement_solver = @service_content.placement_solver
+      placement_hubs = datastores #will be eventually pbm.placement.PlacementHub
+      placement_result = placement_solver.check_compatibility(placement_hubs, profile_id)
+      placement_result.select { |pr| pr.error.empty? }
+    end
     #?? Define pbm_api_uri here
   end
 end
