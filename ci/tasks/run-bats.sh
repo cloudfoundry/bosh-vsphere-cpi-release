0827d0a26a9ecc0aac0c889b84c5c81424cf295a
Tushar Aggarwal <taggarwal@vmware.com>
2018-10-15 17:11:37 -0700

    Expand reserved range for BOSH Network to include 192.168.111.155

--- ci/tasks/run-bats.sh
+++ ci/tasks/run-bats.sh
@@ -17,7 +17,7 @@ bosh int source-ci/ci/shared/bats-spec.yml \
   -v network1-staticIP-1=192.168.111.155 \
   -v network1-vCenterCIDR=192.168.111.0/24 \
   -v network1-staticRange=[192.168.111.155-192.168.111.163] \
-  -v network1-reservedRange="['192.168.111.0-192.168.111.154' , '192.168.111.164-192.168.111.174']" \
+  -v network1-reservedRange="['192.168.111.0-192.168.111.155' , '192.168.111.164-192.168.111.174']" \
   -v network1-vCenterGateway=192.168.111.1 \
   -v network1-vCenterVLAN="VM Network" \
   -v network2-staticIP-1=192.168.111.164 \
