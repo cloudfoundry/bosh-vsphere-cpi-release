0671b2dc9001a251947dc831b5f40637d4049ecc
Imran Mukadam <imran.mukadam@sky.uk>
2019-07-18 20:03:18 +0100

    Add Support for vIDM auth to NSX-T
      [#171032559](https://www.pivotaltracker.com/story/show/171032559)

--- src/vsphere_cpi/lib/cloud/vsphere/nsxt_helpers/nsxt_api_client_builder.rb
+++ src/vsphere_cpi/lib/cloud/vsphere/nsxt_helpers/nsxt_api_client_builder.rb
@@ -3,7 +3,8 @@ module VSphereCloud
     def self.build_api_client(config, logger)
       configuration = NSXT::Configuration.new
       configuration.host = config.host
-
+      configuration.username = config.username
+      configuration.password = config.password
       configuration.logger = logger
       configuration.client_side_validation = false
 
