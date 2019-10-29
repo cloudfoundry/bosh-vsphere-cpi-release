db784f228964cb4ead27e606648268974d1eb03d
Tushar Aggarwal <taggarwal@vmware.com>
2019-10-29 12:00:05 -0700

    Enable cert/key auth in NSXT

--- src/vsphere_cpi/lib/cloud/vsphere/nsxt_helpers/nsxt_api_client_builder.rb
+++ src/vsphere_cpi/lib/cloud/vsphere/nsxt_helpers/nsxt_api_client_builder.rb
@@ -17,6 +17,10 @@ module VSphereCloud
         configuration.verify_ssl = false
         configuration.verify_ssl_host = false
       end
+      if config.private_key
+        configuration.key_file = config.private_key
+        configuration.cert_file = config.certificate
+      end
       NSXT::ApiClient.new(configuration)
     end
   end
