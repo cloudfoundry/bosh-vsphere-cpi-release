b57ce54a6fca9960f4d694d9738beda336afd90d
Tushar Aggarwal <taggarwal@vmware.com>
2019-11-04 16:13:43 -0800

    Add cert/key based auth for NSX-T

--- src/vsphere_cpi/lib/cloud/vsphere/nsxt_helpers/nsxt_api_client_builder.rb
+++ src/vsphere_cpi/lib/cloud/vsphere/nsxt_helpers/nsxt_api_client_builder.rb
@@ -3,23 +3,36 @@ module VSphereCloud
     def self.build_api_client(config, logger)
       configuration = NSXT::Configuration.new
       configuration.host = config.host
-      configuration.username = config.username
-      configuration.password = config.password
+
+      configuration.logger = logger
+      configuration.client_side_validation = false
+
+      # Configure auth key/cert or user/paswords
+      if config.auth_private_key
+        configuration.key_file = config.auth_private_key
+        configuration.cert_file = config.auth_certificate
+        # Are these required with key/cert pair?
+        # configuration.verify_ssl = false
+        # configuration.verify_ssl_host = false
+      else
+        configuration.username = config.username
+        configuration.password = config.password
+      end
+
+      # REMOTE AUTH for NSX-T vIDM Integration
       if config.remote_auth != nil
         configuration.remote_auth = config.remote_auth
       end
-      configuration.logger = logger
-      configuration.client_side_validation = false
+
+      # Root CA Cert
       if ENV['BOSH_NSXT_CA_CERT_FILE']
         configuration.ssl_ca_cert = ENV['BOSH_NSXT_CA_CERT_FILE']
       end
+
+      # SKIP SSL VALIDATION?
       if ENV['NSXT_SKIP_SSL_VERIFY']
         configuration.verify_ssl = false
         configuration.verify_ssl_host = false
-      end
-      if config.private_key
-        configuration.key_file = config.private_key
-        configuration.cert_file = config.certificate
       end
       NSXT::ApiClient.new(configuration)
     end
