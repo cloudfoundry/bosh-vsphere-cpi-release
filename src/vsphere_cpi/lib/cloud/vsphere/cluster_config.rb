f286272dbad861b68d5ddc77dbdbb77d49b30a80
Tushar Aggarwal <taggarwal@vmware.com>
2020-01-31 09:44:28 -0800

    Make host group changes backward compatible
      - Add unit test to check backward compatibility.
      - Add integration test to check backward compatibility.

--- src/vsphere_cpi/lib/cloud/vsphere/cluster_config.rb
+++ src/vsphere_cpi/lib/cloud/vsphere/cluster_config.rb
@@ -8,16 +8,24 @@ module VSphereCloud
     end
 
     def host_group_name
+      if @config.dig('host_group').is_a?(Hash)
         @config.dig('host_group', 'name')
+      else
+        @config['host_group']
+      end
     end
 
     # Returns default ype should if drs_rule is not specified.
     def host_group_drs_rule
-      host_group_drs_rule = @config.dig('host_group', 'drs_rule')
-      unless host_group_drs_rule.nil?
-        return host_group_drs_rule
+      if @config.dig('host_group').is_a?(Hash)
+        host_group_drs_rule = @config.dig('host_group', 'drs_rule')
+        unless host_group_drs_rule.nil?
+          return host_group_drs_rule
+        end
+        'SHOULD'
+      else
+        'MUST'
       end
-      'SHOULD'
     end
 
     def resource_pool
