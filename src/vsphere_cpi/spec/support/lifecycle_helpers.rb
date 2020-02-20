6a6a37e5aeaaba58e3395c8858938c7b2e118b4b
Sanika Gawhane <sgawhane@vmware.com>
2020-02-19 16:16:53 -0800

    Moved helper functions to lifecycle helpers

--- src/vsphere_cpi/spec/support/lifecycle_helpers.rb
+++ src/vsphere_cpi/spec/support/lifecycle_helpers.rb
@@ -685,4 +685,83 @@ module LifecycleHelpers
     end.count
   end
 
+  def verify_tags(vm_mob_id, attached_tags)
+    object_id_hash = { "type" => "VirtualMachine",  "id" => vm_mob_id }
+    object_ids = { "object_ids" => [VSphereAutomation::CIS::VapiStdDynamicID.new(object_id_hash)] }
+    list_attached_tags_on_object = VSphereAutomation::CIS::CisTaggingTagAssociationListAttachedTagsOnObjects.new(object_ids)
+    list_attached_tags_on_objects_result  = tag_association_api.list_attached_tags_on_objects(list_attached_tags_on_object)
+    tags_on_vm_info = list_attached_tags_on_objects_result.value[0]
+    if tags_on_vm_info.nil?
+      tags_on_vm = []
+    else
+      tags_on_vm = tags_on_vm_info.tag_ids
+    end
+    return false unless attached_tags.size == tags_on_vm.size
+    tags_on_vm.each do |tag_id|
+      tag_info = tagging_tag_api.get(tag_id)
+      return false unless attached_tags.include?(tag_info.value.name)
+    end
+    return true
+  end
+
+  def delete_cat_and_tag
+
+    tag_ids.each do |tag_id|
+      tagging_tag_api.delete(tag_id)
+    end
+    cate_ids.each do |cate_id|
+      tagging_category_api.delete(cate_id)
+    end
+  end
+
+  def create_category(category_config_hash)
+    category_create = VSphereAutomation::CIS::CisTaggingCategoryCreate.new(category_config_hash)
+    category_id_info = tagging_category_api.create(category_create)
+    category_id_info.value
+  end
+
+  def create_tag(tag_config_hash)
+    cis_tagging_tag_create = VSphereAutomation::CIS::CisTaggingTagCreate.new(tag_config_hash)
+    tag_id_info = tagging_tag_api.create(cis_tagging_tag_create)
+    tag_id_info.value
+  end
+
+  def fetch_property(key)
+    fail "Missing Environment variable #{key}: #{MISSING_KEY_MESSAGES[key]}" unless (ENV.has_key?(key))
+    value = ENV[key]
+    fail "Environment variable #{key} must not be blank: #{MISSING_KEY_MESSAGES[key]}" if (value =~ /^\s*$/)
+    value
+  end
+
+  def tag_ids
+    return @tag_ids unless @tag_ids.nil?
+    @tag_ids = Array.new
+  end
+
+  def cate_ids
+    return @cate_ids unless @cate_ids.nil?
+    @cate_ids = Array.new
+  end
+
+  def tag_client
+    return @tag_client unless @tag_client.nil?
+    host = fetch_property('BOSH_VSPHERE_CPI_HOST')
+    user = fetch_property('BOSH_VSPHERE_CPI_USER')
+    password = fetch_property('BOSH_VSPHERE_CPI_PASSWORD')
+    cloud_config = OpenStruct.new(vcenter_host: host, vcenter_password: password, vcenter_user: user)
+    @tag_client = VSphereCloud::TaggingTag::AttachTagToVm.InitializeConnection(cloud_config, Bosh::Cpi::Logger.new(STDOUT))
+  end
+
+  def tag_association_api
+    @tag_association_api ||= VSphereAutomation::CIS::TaggingTagAssociationApi.new(tag_client)
+  end
+
+  def tagging_tag_api
+    @tagging_tag_api ||= VSphereAutomation::CIS::TaggingTagApi.new(tag_client)
+  end
+
+  def tagging_category_api
+    @tagging_category_api ||= VSphereAutomation::CIS::TaggingCategoryApi.new(tag_client)
+  end
+
 end
