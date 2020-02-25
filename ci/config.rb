3e71de7e5e6a219c58ce653eae0316c1c3e9ec40
Tushar Aggarwal <taggarwal@vmware.com>
2020-02-25 15:32:48 -0800

    Add vc67 NSXT 23 test
     - Need to test NSXT 2.3 with NSXT 2.5.1 SDK

--- ci/config.rb
+++ ci/config.rb
@@ -52,9 +52,19 @@ end
 
 $pipeline.pool('7.0-NSXT30') do |pool|
   pool.params = {
-      RSPEC_FLAGS: [
-          '--tag nsx_transformers'
-      ].join(' '),
-      NSXT_SKIP_SSL_VERIFY: "true"
+    RSPEC_FLAGS: [
+        '--tag nsx_transformers'
+    ].join(' '),
+    NSXT_SKIP_SSL_VERIFY: "true"
+  }
+end
+
+$pipeline.pool('6.7-NSXT23') do |pool|
+  pool.params = {
+    RSPEC_FLAGS: [
+      '--tag ~nsx_vsphere',
+      '--tag ~host_maintenance',
+    ].join(' '),
+    NSXT_SKIP_SSL_VERIFY: "true"
   }
 end
