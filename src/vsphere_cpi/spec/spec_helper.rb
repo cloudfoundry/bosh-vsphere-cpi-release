cb83e67cb1cc08aa45bd71aa748b37c2231aed4c
Kaiting Chen <ktchen14@gmail.com>
2020-05-22 12:54:25 -0700

    Add VAPIVersionDiscriminant module

--- src/vsphere_cpi/spec/spec_helper.rb
+++ src/vsphere_cpi/spec/spec_helper.rb
@@ -14,6 +14,7 @@ PROJECT_RUBY_VERSION = ENV.fetch('PROJECT_RUBY_VERSION', '2.6.5')
 
 require 'fakefs/spec_helpers'
 
+require 'vapi_version'
 require 'cloud'
 $vc_version = ENV['VC_VERSION'] || '6.5'
 require 'cloud/vsphere'
