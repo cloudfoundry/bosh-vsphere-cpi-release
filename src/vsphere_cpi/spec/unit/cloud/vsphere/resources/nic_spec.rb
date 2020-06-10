9d6a84b5eed5c5be6015070b89dddb3f06777643
Kaiting Chen <kaitingc@vmware.com>
2020-06-10 11:30:47 -0700

    Refactor nic_spec.rb

--- src/vsphere_cpi/spec/unit/cloud/vsphere/resources/nic_spec.rb
+++ src/vsphere_cpi/spec/unit/cloud/vsphere/resources/nic_spec.rb
@@ -1,48 +1,44 @@
 require 'spec_helper'
 
-module VSphereCloud::Resources
-  describe Nic do
-    describe '#create_nic_config_spec' do
-      subject(:vsphere_cloud) { Cloud.new(config) }
+describe VSphereCloud::Resources::Nic do
+  describe '#create_nic_config_spec' do
+    let(:cloud_searcher) { instance_double(VSphereCloud::CloudSearcher) }
+    let(:network) do
+      instance_double(VSphereCloud::Resources::Network, network_id: network_id, nic_backing: nil)
+    end
+    let(:network_id) { :fake }
 
-      let(:config) { { fake: 'config' } }
-      let(:nid) { :fake }
-      let(:dvs_hash) { {nid => v_network_name} }
-      let(:dvs_index) { {} }
-      let(:cloud_searcher) { instance_double('VSphereCloud::CloudSearcher') }
-      context 'using a distributed switch' do
-        let(:v_network_name) { 'fake_network1' }
-        let(:network) { instance_double(VSphereCloud::Resources::Network, nic_backing: 'fake-backing') }
+    let(:dvs_index) { {} }
+    let(:create_vnic_args) do
+      [cloud_searcher, 'fake-v-network-name', network, 'fake_controller_key', dvs_index]
+    end
 
-        it 'sets correct operation in device config spec' do
-          allow(network).to receive(:network_id).and_return(nid)
-          virtual_nic = Nic.create_virtual_nic(cloud_searcher, v_network_name, network, 'fake_controller_key', dvs_index)
-          device_config_spec = VM.create_add_device_spec(virtual_nic)
+    it 'sets the correct operation in the device config spec' do
+      vnic = described_class.create_virtual_nic(*create_vnic_args)
+      device_config_spec = VSphereCloud::Resources::VM.create_add_device_spec(vnic)
 
-          expect(device_config_spec.operation).to eq(VimSdk::Vim::Vm::Device::VirtualDeviceSpec::Operation::ADD)
-          expect(dvs_index).to eq(dvs_hash)
-        end
+      expect(device_config_spec.operation).to eq(
+        VimSdk::Vim::Vm::Device::VirtualDeviceSpec::Operation::ADD)
+      expect(dvs_index).to eq(network_id => 'fake-v-network-name')
+    end
 
-        context 'when network ID returned is nil' do
-          it 'does not populate dvs index' do
-            allow(network).to receive(:network_id).and_return(nil)
-            virtual_nic = Nic.create_virtual_nic(cloud_searcher, v_network_name, network, 'fake_controller_key', dvs_index)
-            device_config_spec = VM.create_add_device_spec(virtual_nic)
+    context 'when the network id is nil' do
+      let(:network_id) { nil }
 
-            expect(device_config_spec.operation).to eq(VimSdk::Vim::Vm::Device::VirtualDeviceSpec::Operation::ADD)
-            expect(dvs_index).to eq({})
-          end
+      it "doesn't populate dvs index" do
+        expect do
+          described_class.create_virtual_nic(*create_vnic_args)
+        end.to_not change { dvs_index }
+      end
+    end
 
-        end
+    context 'when the network is nil' do
+      let(:network) { nil }
 
-        context 'when Passed network is nil' do
-          let(:network) { nil }
-          it 'raises Invalid network error' do
-            expect do
-              Nic.create_virtual_nic(cloud_searcher, v_network_name, network, 'fake_controller_key', dvs_index)
-              end.to raise_error StandardError, /Invalid network/
-          end
-        end
+      it 'raises an invalid network error' do
+        expect do
+          described_class.create_virtual_nic(*create_vnic_args)
+        end.to raise_error /Invalid network/
       end
     end
   end
