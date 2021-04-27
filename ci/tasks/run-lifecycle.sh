d278d52f19663f3b3c7bf40f5f93c2cb028eb42c
Maria Shaldybin <mariash@vmware.com>
2021-04-27 16:34:17 -0700

    Use sshuttle instead of http(s)_proxy in lifecycle tests
    So that we can test NSXT manager with SSL
    
    Co-authored-by: Maria Shaldybin <mariash@vmware.com>
    Co-authored-by: Julian Hjortshoj <hjortshojj@vmware.com>

--- ci/tasks/run-lifecycle.sh
+++ ci/tasks/run-lifecycle.sh
@@ -4,7 +4,7 @@ set -e
 
 source bosh-cpi-src/.envrc
 
-source source-ci/ci/shared/tasks/setup-env-proxy.sh
+source environment/metadata
 
 stemcell_dir="$( cd stemcell && pwd )"
 export BOSH_VSPHERE_STEMCELL=${stemcell_dir}/stemcell.tgz
@@ -35,6 +35,7 @@ if [ $BOSH_VSPHERE_CPI_NSXT_HOST != "null" ]; then
   sshpass -p "vcpi" ssh -o StrictHostKeyChecking=no "vcpi@${BOSH_VSPHERE_JUMPER_HOST}" -C "openssl s_client -showcerts -connect $BOSH_VSPHERE_CPI_NSXT_HOST:443 </dev/null 2>/dev/null" | openssl x509 -outform PEM > $BOSH_NSXT_CA_CERT_FILE
   # The certificate's subject name is nsxt-manager so SSL validation fails when using the IP address
   echo "${BOSH_VSPHERE_CPI_NSXT_HOST} nsxt-manager" >> /etc/hosts
+  sshpass -p "vcpi" sshuttle -r "vcpi@${BOSH_VSPHERE_JUMPER_HOST}" 192.168.111.0/22 --no-latency-control 2>&1 &
   export BOSH_VSPHERE_CPI_NSXT_HOST="https://nsxt-manager"
 fi
 
