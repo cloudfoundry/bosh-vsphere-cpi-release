97bde49b79321f8d23f44efd18117ca31e1c66ca
Maria Shaldybin <mariash@vmware.com>
2021-04-29 12:13:39 -0700

    Fix CI scripting for NSX-V tests
    - we removed the proxy settings from the lifecycle script, but only
    replced them with sshuttle when NSX-T was set.
    - this change restores the proxy when NSX-T isn't present in the
    environment.
    
    Co-authored-by: Maria Shaldybin <mariash@vmware.com>
    Co-authored-by: Michael Oleske <moleske@pivotal.io>
    Co-authored-by: Julian Hjortshoj <hjortshojj@vmware.com>

--- ci/tasks/run-lifecycle.sh
+++ ci/tasks/run-lifecycle.sh
@@ -4,7 +4,19 @@ set -e
 
 source bosh-cpi-src/.envrc
 
-source environment/metadata
+
+if [ $BOSH_VSPHERE_CPI_NSXT_HOST != "null" ]; then
+  source environment/metadata
+  export BOSH_NSXT_CA_CERT_FILE=$(realpath $PWD/nsxt-manager-cert.pem)
+  # To get the cert from nsxt-manager, we run openssl on the jump box, and then pipe that result into a local openssl command that reformats it into PEM
+  sshpass -p "vcpi" ssh -o StrictHostKeyChecking=no "vcpi@${BOSH_VSPHERE_JUMPER_HOST}" -C "openssl s_client -showcerts -connect $BOSH_VSPHERE_CPI_NSXT_HOST:443 </dev/null 2>/dev/null" | openssl x509 -outform PEM > $BOSH_NSXT_CA_CERT_FILE
+  # The certificate's subject name is nsxt-manager so SSL validation fails when using the IP address
+  echo "${BOSH_VSPHERE_CPI_NSXT_HOST} nsxt-manager" >> /etc/hosts
+  sshpass -p "vcpi" sshuttle -r "vcpi@${BOSH_VSPHERE_JUMPER_HOST}" 192.168.111.0/22 --no-latency-control 2>&1 &
+  export BOSH_VSPHERE_CPI_NSXT_HOST="https://nsxt-manager"
+else
+  source source-ci/ci/shared/tasks/setup-env-proxy.sh
+fi
 
 stemcell_dir="$( cd stemcell && pwd )"
 export BOSH_VSPHERE_STEMCELL=${stemcell_dir}/stemcell.tgz
@@ -29,15 +41,6 @@ install_iso9660wrap() {
 
 install_iso9660wrap
 
-if [ $BOSH_VSPHERE_CPI_NSXT_HOST != "null" ]; then
-  export BOSH_NSXT_CA_CERT_FILE=$(realpath $PWD/nsxt-manager-cert.pem)
-  # To get the cert from nsxt-manager, we run openssl on the jump box, and then pipe that result into a local openssl command that reformats it into PEM
-  sshpass -p "vcpi" ssh -o StrictHostKeyChecking=no "vcpi@${BOSH_VSPHERE_JUMPER_HOST}" -C "openssl s_client -showcerts -connect $BOSH_VSPHERE_CPI_NSXT_HOST:443 </dev/null 2>/dev/null" | openssl x509 -outform PEM > $BOSH_NSXT_CA_CERT_FILE
-  # The certificate's subject name is nsxt-manager so SSL validation fails when using the IP address
-  echo "${BOSH_VSPHERE_CPI_NSXT_HOST} nsxt-manager" >> /etc/hosts
-  sshpass -p "vcpi" sshuttle -r "vcpi@${BOSH_VSPHERE_JUMPER_HOST}" 192.168.111.0/22 --no-latency-control 2>&1 &
-  export BOSH_VSPHERE_CPI_NSXT_HOST="https://nsxt-manager"
-fi
 
 pushd bosh-cpi-src/src/vsphere_cpi
   bundle install
