cbd1cb0a36c895f8b2b4e58d56e44f3714e81c34
Gary Liu <garyliu@vmware.com>
2021-05-24 12:27:39 -0700

    CI: lifecycle tests: sshuttle+squid → OpenVPN
    We switch from using a combination of sshuttle+squid to OpenVPN. We hope
    this will accomplish two things:
    
    1. Make the lifecycle tests more resilient
    1. Speed up our lifecycle tests
    
    The lifecycle tests have been failing at a prodigious rate since we
    switched to `sshuttle` as a transport mechanism to the vSphere
    environment. As an extreme example, for the job
    [lifecycle-7.0-nsxt30](https://runway-ci.eng.vmware.com/teams/dap-tas-anycloud/pipelines/vsphere-cpi/jobs/lifecycle-7.0-nsxt30),
    runs 66 through 75 experienced only a 10% success rate.
    
    Also, the tests have been taking longer. For example, run 69 took 175
    minutes. After switch to OpenVPN, a lifecycle run took 109 minutes: a
    60% increase in speed!
    
    We note that there are 7 lifecycle tests, and 107 workers, so the
    probably that any two jobs run on the same worker is ~6%. We expect runs
    that share the same worker to fail because they'll both contend for the
    tunneling device & routes.
    
    [#178271765] Fix repeated vSphere CPI CI lifecycle failures
    
    Signed-off-by: Brian Cunnie <bcunnie@vmware.com>

--- ci/tasks/run-lifecycle.sh
+++ ci/tasks/run-lifecycle.sh
@@ -5,16 +5,40 @@ set -e
 source bosh-cpi-src/.envrc
 source environment/metadata
 
+# get the tun device; we need it to establish OpenVPN
+mkdir -p /mnt/dev
+mount -t devtmpfs devtmpfs /mnt/dev
+ln -s /mnt/dev/net /dev/net
+
+# install OpenVPN
+apt update && apt install -y openvpn # TODO move this into the Docker image and delete this line
+
+# configure passwordless and sshpass-less ssh to VCPI jumpbox
+mkdir -p ~/.ssh
+ssh-keygen -t ed25519 -f ~/.ssh/vcpi -N ""
+cat > ~/.ssh/config <<EOF
+Host jumpbox
+        HostName ${BOSH_VSPHERE_JUMPER_HOST}
+        User vcpi
+        IdentityFile ~/.ssh/vcpi
+        StrictHostKeyChecking no
+EOF
+chmod -R go-rwx ~/.ssh
+sshpass -p vcpi scp ~/.ssh/vcpi.pub jumpbox:.ssh/authorized_keys
+
+# grab OpenVPN client configuration & update it with VCPI jumpbox IP
+scp jumpbox:client.ovpn /etc/openvpn/
+sed --in-place "s/jumper.vcpi-nimbus.local/${BOSH_VSPHERE_JUMPER_HOST}/" /etc/openvpn/client.ovpn
+openvpn --cd /etc/openvpn --config /etc/openvpn/client.ovpn &
+sleep 6 # OpenVPN needs ~3 secs to establish its connection
+
 if [ "$BOSH_VSPHERE_CPI_NSXT_HOST" != "null" ]; then
   export BOSH_NSXT_CA_CERT_FILE=$(realpath $PWD/nsxt-manager-cert.pem)
   # To get the cert from nsxt-manager, we run openssl on the jump box, and then pipe that result into a local openssl command that reformats it into PEM
-  sshpass -p "vcpi" ssh -o StrictHostKeyChecking=no "vcpi@${BOSH_VSPHERE_JUMPER_HOST}" -C "openssl s_client -showcerts -connect $BOSH_VSPHERE_CPI_NSXT_HOST:443 </dev/null 2>/dev/null" | openssl x509 -outform PEM > $BOSH_NSXT_CA_CERT_FILE
+  openssl s_client -showcerts -connect $BOSH_VSPHERE_CPI_NSXT_HOST:443 </dev/null 2>/dev/null | openssl x509 -outform PEM > $BOSH_NSXT_CA_CERT_FILE
   # The certificate's subject name is nsxt-manager so SSL validation fails when using the IP address
   echo "${BOSH_VSPHERE_CPI_NSXT_HOST} nsxt-manager" >> /etc/hosts
-  sshpass -p "vcpi" sshuttle -r "vcpi@${BOSH_VSPHERE_JUMPER_HOST}" 192.168.111.0/22 --no-latency-control 2>&1 &
   export BOSH_VSPHERE_CPI_NSXT_HOST="https://nsxt-manager"
-else
-  source source-ci/ci/shared/tasks/setup-env-proxy.sh
 fi
 
 stemcell_dir="$( cd stemcell && pwd )"
@@ -38,10 +62,22 @@ install_iso9660wrap() {
   popd
 }
 
-install_iso9660wrap
+cleanup() {
+  set +e # if you hit an error, continue; we're trying to clean up as much we can
+  kill %openvpn
+  fg %openvpn # make sure OpenVPN shutdown has completed
+  rm /dev/net
+  umount /mnt/dev
+  set -e
+}
 
+install_iso9660wrap
 
 pushd bosh-cpi-src/src/vsphere_cpi
   bundle install
+  set +e # do NOT prematurely exit if rspec fails—we want to tear down OpenVPN before we exit
   bundle exec rspec ${RSPEC_FLAGS} --require ./spec/support/verbose_formatter.rb --format VerboseFormatter spec/integration
+  set -e
 popd
+
+cleanup
