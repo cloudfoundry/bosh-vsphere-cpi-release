f1bff5c5da6a5622d6127f6a32844e3361c613ce
Maria Shaldybin <mariash@vmware.com>
2021-06-03 14:55:44 -0700

    Freshen revisions of vSphere and NSX-T we test
    - NSX-T 2.4 is out of support (EOGS 2020-09-07, EOTG 2021-09-07)
    - NSX-T 3.1 is released
    - vSphere 6.0 is out of support (EOGS 2020-03-12, EOTG 2022-03-12)
    - CVDS tests now also use NSX-T 3.1
    - use Xenial stemcell (621.125) instead of horribly-old Trusty
    - bumped "old BOSH release" to something not-quite-so-old (`v271.7.0`)
    - we eliminated the Concourse groups; we don't use them, and they're
      hard to maintain
    
    [#178147374](https://www.pivotaltracker.com/story/show/178147374)
    
    Co-Authored-by: Julian Hjortshoj <hjortshojj@vmware.com>
    Co-Authored-by: Brian Cunnie <bcunnie@vmware.com>

--- ci/config.rb
+++ ci/config.rb
@@ -1,17 +1,3 @@
-$pipeline.pool('6.0-nsxv') do |pool|
-  pool.params = {
-    RSPEC_FLAGS: [
-      '--tag ~nvds',
-      '--tag ~cvds',
-      '--tag ~spbm_encryption',
-      '--tag ~attach_tag',
-      '--tag ~host_maintenance',
-      '--tag ~nsxt_all',
-    ].join(' '),
-    NSXT_SKIP_SSL_VERIFY: "true"
-  }
-end
-
 $pipeline.pool('6.5-nsxt25') do |pool|
   pool.params = {
     RSPEC_FLAGS: [
@@ -19,19 +5,6 @@ $pipeline.pool('6.5-nsxt25') do |pool|
       '--tag ~disk_migration',
       '--tag ~nsxv',
       '--tag ~host_maintenance',
-      '--tag ~nsxt_policy',
-    ].join(' '),
-    NSXT_SKIP_SSL_VERIFY: "true"
-  }
-end
-
-$pipeline.pool('6.5-nsxt24') do |pool|
-  pool.params = {
-    RSPEC_FLAGS: [
-      '--tag ~cvds',
-      '--tag nvds',
-      '--tag nsxt_all',
-      '--tag vsphere_networking',
       '--tag ~nsxt_policy',
     ].join(' '),
     NSXT_SKIP_SSL_VERIFY: "true"
@@ -50,19 +23,6 @@ $pipeline.pool('6.7-nsxt25') do |pool|
   }
 end
 
-$pipeline.pool('6.7-nsxt24') do |pool|
-  pool.params = {
-    RSPEC_FLAGS: [
-      '--tag ~cvds',
-      '--tag nvds',
-      '--tag nsxt_all',
-      '--tag vsphere_networking',
-      '--tag ~nsxt_policy',
-    ].join(' '),
-    NSXT_SKIP_SSL_VERIFY: "true"
-  }
-end
-
 $pipeline.pool('7.0-nsxt30') do |pool|
   pool.params = {
     RSPEC_FLAGS: [
@@ -73,7 +33,17 @@ $pipeline.pool('7.0-nsxt30') do |pool|
   }
 end
 
-$pipeline.pool('7.0-nsxt30-cvds') do |pool|
+$pipeline.pool('7.0-nsxt31') do |pool|
+  pool.params = {
+    RSPEC_FLAGS: [
+      '--tag ~cvds',
+      '--tag ~nsxv',
+      '--tag ~host_maintenance',
+    ].join(' '),
+  }
+end
+
+$pipeline.pool('7.0-nsxt31-cvds') do |pool|
   pool.params = {
     RSPEC_FLAGS: [
       '--tag ~nvds',
