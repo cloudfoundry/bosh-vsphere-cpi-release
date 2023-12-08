0371832f5fbc07a92d1dac67351f45dc4bdb6a95
Joseph Palermo <jpalermo@pivotal.io>
2023-12-07 17:33:55 -0800

    Update bats config generation to account for changes in bats

--- ci/tasks/run-bats-cvds.sh
+++ ci/tasks/run-bats-cvds.sh
@@ -5,7 +5,6 @@ set -e
 : ${STEMCELL_NAME:?}
 : ${BAT_RSPEC_FLAGS:?}
 : ${BAT_INFRASTRUCTURE:?}
-: ${BAT_NETWORKING:?}
 
 source source-ci/ci/shared/tasks/setup-env-proxy.sh
 
@@ -25,11 +24,11 @@ bosh int source-ci/ci/shared/bats-spec.yml \
   -v network2-staticRange="['30.0.0.164-30.0.0.174']" \
   -v network2-reservedRange="['30.0.0.0-30.0.0.163']" \
   -v network2-vCenterGateway='30.0.0.1' \
-  -v network2-vCenterVLAN="nsxt-switch1" > bats-config/bats-config.yml
+  -v network2-vCenterVLAN="nsxt-switch1" \
+  -v ssh_private_key="$(bosh int director-state/creds.yml --path=/jumpbox_ssh/private_key)" \
+  -v ssh_public_key="$(bosh int director-state/creds.yml --path=/jumpbox_ssh/public_key)" > bats-config/bats-config.yml
 
 source director-state/director.env
-export BAT_PRIVATE_KEY="$(bosh int director-state/creds.yml --path=/jumpbox_ssh/private_key)"
-export BAT_DNS_HOST="${BOSH_ENVIRONMENT}"
 export BAT_STEMCELL=$(realpath stemcell/*.tgz)
 export BAT_DEPLOYMENT_SPEC=$(realpath bats-config/bats-config.yml)
 export BAT_BOSH_CLI=$(which bosh)
--- ci/tasks/run-bats-policy.sh
+++ ci/tasks/run-bats-policy.sh
@@ -5,7 +5,6 @@ set -e
 : ${STEMCELL_NAME:?}
 : ${BAT_RSPEC_FLAGS:?}
 : ${BAT_INFRASTRUCTURE:?}
-: ${BAT_NETWORKING:?}
 
 source source-ci/ci/shared/tasks/setup-env-proxy.sh
 
@@ -25,11 +24,11 @@ bosh int source-ci/ci/shared/bats-spec.yml \
   -v network2-staticRange=[30.0.3.164-30.0.3.174] \
   -v network2-reservedRange="['30.0.0.0-30.0.2.255']" \
   -v network2-vCenterGateway=30.0.0.1 \
-  -v network2-vCenterVLAN="vcpi-segment" > bats-config/bats-config.yml
+  -v network2-vCenterVLAN="vcpi-segment" \
+  -v ssh_private_key="$(bosh int director-state/creds.yml --path=/jumpbox_ssh/private_key)" \
+  -v ssh_public_key="$(bosh int director-state/creds.yml --path=/jumpbox_ssh/public_key)" > bats-config/bats-config.yml
 
 source director-state/director.env
-export BAT_PRIVATE_KEY="$(bosh int director-state/creds.yml --path=/jumpbox_ssh/private_key)"
-export BAT_DNS_HOST="${BOSH_ENVIRONMENT}"
 export BAT_STEMCELL=$(realpath stemcell/*.tgz)
 export BAT_DEPLOYMENT_SPEC=$(realpath bats-config/bats-config.yml)
 export BAT_BOSH_CLI=$(which bosh)
