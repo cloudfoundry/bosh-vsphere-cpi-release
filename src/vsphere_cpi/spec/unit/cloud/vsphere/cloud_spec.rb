402bfc8e47631fe3bfe533174edb06a1eab5a24b
Brian Cunnie <brian.cunnie@broadcom.com>
2024-01-08 15:50:51 -0800

    Remove specs for deprecated CPI network methods
    This included all `xdescribe`'d tests in the project.  We did not delete
    the method calls (`create_network` and `delete_network`) but they only
    work for the _old_ NSX-T Manager API, and we don't believe they're used
    by anyone.
    
    Co-authored-by: Chris Selzo <chris.selzo@broadcom.com>

--- src/vsphere_cpi/spec/unit/cloud/vsphere/cloud_spec.rb
+++ src/vsphere_cpi/spec/unit/cloud/vsphere/cloud_spec.rb
@@ -2658,63 +2658,6 @@ module VSphereCloud
       end
     end
 
-    # NOT NEEDED - FEATURE DISCONTINUED
-    xdescribe '#create_network' do
-      let(:network_definition) { {
-                                 'range' => '192.168.111.0/24',
-                                 'gateway' => '192.168.111.1',
-                                 'cloud_properties' => {
-                                     't0_router_id' => 't0-router-id',
-                                     't1_name' => 'router-name',
-                                     'transport_zone_id' => 'zone-id',
-                                     'switch_name' => 'switch-name',
-                                 } } }
-      let(:nsxt_provider) { instance_double(VSphereCloud::NSXTProvider) }
-      let(:switch_provider) { instance_double(VSphereCloud::NSXTSwitchProvider) }
-      let(:router_provider) { instance_double(VSphereCloud::NSXTRouterProvider) }
-      let(:ip_block_provider) { instance_double(VSphereCloud::NSXTIpBlockProvider) }
-      let(:nsxt_enabled) { true }
-      let(:network_result) { instance_double(VSphereCloud::ManagedNetwork) }
-      let(:network) { instance_double(VSphereCloud::Network) }
-      let(:nsxt_client) { instance_double(NSXT::ApiClient) }
-
-      before do
-        allow(VSphereCloud::NSXTApiClientBuilder).to receive(:build_api_client)
-          .with(any_args).and_return(nsxt_client)
-        allow(VSphereCloud::NSXTProvider).to receive(:new)
-          .with(any_args).and_return(nsxt_provider)
-        allow(VSphereCloud::NSXTSwitchProvider).to receive(:new)
-          .with(any_args).and_return(switch_provider)
-        allow(VSphereCloud::NSXTRouterProvider).to receive(:new)
-          .with(any_args).and_return(router_provider)
-        allow(VSphereCloud::NSXTIpBlockProvider).to receive(:new)
-           .with(any_args).and_return(ip_block_provider)
-      end
-
-      context 'when nsxt disabled' do
-        let(:nsxt_enabled) { false }
-
-        it 'raises an error' do
-          expect{
-            vsphere_cloud.create_network(network_definition)
-          }.to raise_error('NSXT must be enabled in CPI to use create_network')
-        end
-      end
-
-      context 'when nsxt enabled' do
-        let(:network_model) { instance_double(VSphereCloud::NetworkDefinition) }
-        it 'creates a network' do
-          expect(VSphereCloud::NetworkDefinition).to receive(:new)
-            .with(network_definition).and_return(network_model)
-          expect(VSphereCloud::Network).to receive(:new)
-            .with(switch_provider, router_provider, ip_block_provider).and_return(network)
-          expect(network).to receive(:create)
-            .with(network_model).and_return(network_result)
-          expect(vsphere_cloud.create_network(network_definition)).to eq(network_result)
-        end
-      end
-    end
-
     # TEST ERROR RETURNS ON DISCONTINUED FEATURE
     describe '#create_network errors' do
       let(:network_definition) { {
@@ -2768,66 +2711,6 @@ module VSphereCloud
           expect{
             vsphere_cloud.create_network(network_definition)
           }.to raise_error('create_network is not supported for the NSXT Policy API')
-        end
-      end
-    end
-
-    # NOT NEEDED - FEATURE DISCONTINUED
-    xdescribe '#delete_network' do
-      let(:nsxt_provider) { instance_double(VSphereCloud::NSXTProvider) }
-      let(:nsxt_manager_client) { instance_double(NSXT::ApiClient) }
-      let(:switch_provider) { instance_double(VSphereCloud::NSXTSwitchProvider) }
-      let(:router_provider) { instance_double(VSphereCloud::NSXTRouterProvider) }
-      let(:ip_block_provider) { instance_double(VSphereCloud::NSXTIpBlockProvider) }
-      let(:network) { instance_double(VSphereCloud::Network) }
-
-      before do
-        allow(VSphereCloud::NSXTApiClientBuilder).to receive(:build_api_client)
-         .with(any_args).and_return(nsxt_manager_client)
-        allow(VSphereCloud::NSXTProvider).to receive(:new)
-         .with(any_args).and_return(nsxt_provider)
-        allow(VSphereCloud::NSXTIpBlockProvider).to receive(:new)
-          .with(any_args).and_return(ip_block_provider)
-      end
-
-      context 'when nsxt disabled' do
-        let(:nsxt_enabled) { false }
-
-        it 'raises an error' do
-          expect{
-            vsphere_cloud.delete_network('switch-id')
-          }.to raise_error('NSXT must be enabled in CPI to use delete_network')
-        end
-      end
-
-      context 'when nsxt enabled with policy API' do
-        let(:nsxt_enabled) { true }
-        let(:nsxt) { instance_double(VSphereCloud::NSXTConfig, default_vif_type: 'vif_type', use_policy_api?: true, auth_private_key: nil)}
-
-        it 'raises an error' do
-          expect{
-            vsphere_cloud.delete_network(network_definition)
-          }.to raise_error('create_network is not supported for the NSXT Policy API')
-        end
-      end
-
-      context 'when nsxt enabled' do
-        let(:nsxt_enabled) { true }
-        let(:switch_provider) { instance_double(VSphereCloud::NSXTSwitchProvider) }
-
-        before do
-          allow(NSXTSwitchProvider).to receive(:new)
-            .and_return(switch_provider)
-          allow(VSphereCloud::NSXTRouterProvider).to receive(:new)
-            .with(any_args).and_return(router_provider)
-        end
-
-        it 'deletes a network' do
-          expect(VSphereCloud::Network).to receive(:new)
-            .with(switch_provider, router_provider, ip_block_provider).and_return(network)
-          expect(network).to receive(:destroy)
-            .with('switch-id')
-          vsphere_cloud.delete_network('switch-id')
         end
       end
     end
