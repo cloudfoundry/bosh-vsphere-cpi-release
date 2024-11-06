300a7ad936041717f9d458aa87c2d94ab2ba8b42
Brian Upton <brian.upton@broadcom.com>
2024-11-06 12:09:24 -0800

    Build artifacts as needed

--- ci/tasks/build-candidate.sh
+++ ci/tasks/build-candidate.sh
@@ -2,11 +2,8 @@
 
 set -e
 
-semver=$(cat version-semver/version)
-
 echo Creating BOSH vSphere CPI release ... 1>&2
 bosh create-release \
   --dir bosh-cpi-src \
   --name bosh-vsphere-cpi \
-  --version "$semver" \
-  --tarball "dev-artifacts/bosh-vsphere-cpi-$semver.tgz"
+  --tarball "cpi-release/bosh-vsphere-cpi-dev.tgz"
