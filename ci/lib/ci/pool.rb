dc992afafdcc1f07c644fa8ef4a86130b9a92a69
Nishad Mathur <nishad.mathur@broadcom.com>
2025-07-30 12:02:07 -0700

    Update CI to use shepherd and resolve several small issues with environments
    Co-authored-by: Julian Hjortshoj <julian.hjortshoj@broadcom.com>

--- ci/lib/ci/pool.rb
+++ ci/lib/ci/pool.rb
@@ -1,15 +1,20 @@
 class Pool
-  attr_reader :name
-
-  attr_writer :params
+  attr_reader :name, :generate_lifecycle_test
+  attr_accessor :params,:gating
 
   def initialize(name)
     @name = name
+    @params = {}
+    @generate_lifecycle_test = true
+    @gating = true
   end
 
-  def params
-    @params ||= {}
+  def skip_lifecycle_test
+    @generate_lifecycle_test = false
+    @gating = false
   end
 
+  alias generate_lifecycle_test? generate_lifecycle_test
+  alias gating? gating
   alias to_s name
 end
