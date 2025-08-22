28b27b2d6a85564fbb9a91095c551cd4facc44d2
Nishad Mathur <nishad.mathur@broadcom.com>
2025-08-22 12:04:30 -0700

    Change pools
    Co-authored-by: Clay Kauzlaric <clay.kauzlaric@broadcom.com>

--- ci/config.rb
+++ ci/config.rb
@@ -1,4 +1,4 @@
-$pipeline.pool('7.0-nsxt30') do |pool|
+$pipeline.pool('7.0-nsxt32-nvds') do |pool|
   pool.params = {
     RSPEC_FLAGS: [
       '--tag ~cvds',
@@ -8,17 +8,7 @@ $pipeline.pool('7.0-nsxt30') do |pool|
   }
 end
 
-$pipeline.pool('7.0-nsxt31') do |pool|
-  pool.params = {
-    RSPEC_FLAGS: [
-      '--tag ~cvds',
-      '--tag ~nsxv',
-      '--tag ~host_maintenance',
-    ].join(' '),
-  }
-end
-
-$pipeline.pool('7.0-nsxt31-cvds') do |pool|
+$pipeline.pool('7.0-nsxt32-cvds') do |pool|
   pool.params = {
     RSPEC_FLAGS: [
       '--tag ~nvds',
@@ -29,40 +19,22 @@ $pipeline.pool('7.0-nsxt31-cvds') do |pool|
   }
 end
 
-$pipeline.pool('7.0-nsxt31-policy') do |pool|
-  pool.skip_lifecycle_test
-end
-
-$pipeline.pool('8.0-nsxt40-cvds') do |pool|
+$pipeline.pool('7.0-nsxt32-policy') do |pool|
   pool.params = {
     RSPEC_FLAGS: [
+      '--tag ~nsxv',
       '--tag ~nvds',
       '--tag cvds',
       '--tag nsxt_all',
-      '--tag ~host_maintenance',
     ].join(' '),
   }
 end
 
-$pipeline.pool('8.0-nsxt41-cvds') do |pool|
+$pipeline.pool('8.0-nsxt42-policy') do |pool|
   pool.params = {
     RSPEC_FLAGS: [
+      '--tag ~nsxv',
       '--tag ~nvds',
-      '--tag cvds',
-      '--tag nsxt_all',
-      '--tag ~host_maintenance',
-    ].join(' '),
-  }
-end
-
-$pipeline.pool('8.pre-release-nsxt41-cvds') do |pool|
-  pool.gating = false
-  pool.params = {
-    RSPEC_FLAGS: [
-      '--tag ~nvds',
-      '--tag cvds',
-      '--tag nsxt_all',
-      '--tag ~host_maintenance',
     ].join(' '),
   }
 end
