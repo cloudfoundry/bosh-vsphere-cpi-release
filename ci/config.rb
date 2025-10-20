b31ca0cc47a683879fe26603324c67821f0542dd
Brian Upton <brian.upton@broadcom.com>
2025-10-20 16:26:24 -0700

    Run all tests on each environment type
    Rather than focusing the tests for certain environments, attempt to run all
    tests on each environment. The only exception is that we do not run nvds tests
    on cvds environments, or vice versa
    
    [TNZ-58625]
    
    Signed-off-by: Nishad Mathur <nishad.mathur@broadcom.com>
    Co-authored-by: Aram Price <aram.price@broadcom.com>

--- ci/config.rb
+++ ci/config.rb
@@ -1,8 +1,5 @@
 # TODO(nm,ck):
 #  - reenable host maintenance tests
-#  - run lifecycle tests with policy api
-#  - run full suite against policy/cvds since its only run against NVDS today
-#  - make sure the cvds tests all run `--tag vsphere_networking`
 
 $pipeline.pool('7.0-nsxt32-nvds') do |pool|
   pool.params = {
@@ -18,22 +15,15 @@ $pipeline.pool('7.0-nsxt32-cvds') do |pool|
     RSPEC_FLAGS: [
       '--tag ~nvds',
       '--tag ~host_maintenance',
-      '--tag cvds',
-      '--tag nsxt_all',
-      '--tag vsphere_networking',
     ].join(' ')
   }
 end
 
 $pipeline.pool('7.0-nsxt32-policy') do |pool|
-  # TODO(nm,ck): We want to come back and make these green as we dont have time to do this before comet ships
-  pool.skip_lifecycle_test
   pool.params = {
     RSPEC_FLAGS: [
       '--tag ~nvds',
       '--tag ~host_maintenance',
-      '--tag cvds',
-      '--tag nsxt_all',
     ].join(' ')
   }
 end
@@ -43,16 +33,12 @@ $pipeline.pool('8.0-nsxt42-cvds') do |pool|
     RSPEC_FLAGS: [
       '--tag ~nvds',
       '--tag ~host_maintenance',
-      '--tag cvds',
-      '--tag nsxt_all',
     ].join(' ')
   }
 end
 
 
 $pipeline.pool('8.0-nsxt42-policy') do |pool|
-  # TODO(nm,ck): We want to come back and make these green as we dont have time to do this before comet ships
-  pool.skip_lifecycle_test
   pool.params = {
     RSPEC_FLAGS: [
       '--tag ~nvds',
