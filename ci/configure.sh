6ea4d1d79e8bf998fb8820d521e4956f9856c682
Aram Price <aram.price@broadcom.com>
2025-10-28 16:35:23 -0700

    Remove NSXT exclusion tags for policy as we now match the nvds/cvds segment names
    Co-authored-by: Nishad Mathur <nishad.mathur@broadcom.com>

--- ci/configure.sh
+++ ci/configure.sh
@@ -5,4 +5,6 @@ SCRIPT_DIR="$( cd "$( dirname "${0}" )" && pwd )"
 
 export CONCOURSE="${CONCOURSE_TARGET:-bosh-ecosystem}"
 cd "${SCRIPT_DIR}"
-rake pipeline
+
+bundle install
+bundle exec rake pipeline
